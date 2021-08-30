fee8b9db249f7b32866f284071d3341f7e8fe176
AndrewGlago <andrewglago1@gmail.com>
2021-08-30 00:21:24 +0000

    migrate CRA template to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,29 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				craPrimary: '#282c34',
+			},
+			textColor: {
+				craLink: '#61dafb',
+			},
+			fontFamily: {
+				craFont: [
+					'-apple - system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'Oxygen',
+					'Ubuntu',
+					'Cantarell',
+					'Fira Sans',
+					'Droid Sans',
+					'Helvetica Neue',
+					'sans - serif',
+				],
+			},
+		},
 	},
 	variants: {
 		extend: {},
