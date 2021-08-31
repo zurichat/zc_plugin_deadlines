50038fe801ea1d95f2a3ddc4e53c1988abfb4e3b
AndrewGlago <andrewglago1@gmail.com>
2021-08-31 01:18:15 +0000

    add backend repo lint config and set mocha var in env

---
+++ Backend/.eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+		es2021: true,
+		mocha: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	plugins: ['import', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': ['error', { endOfLine: 'auto' }],
+		'arrow-body-style': 'off',
+		'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
+		'prefer-const': ['error'],
+		'no-var': ['error'],
+		'no-new-object': ['error'],
+		'object-shorthand': ['error'],
+		'quote-props': ['error', 'as-needed'],
+		'prefer-object-spread': ['error'],
+		'array-callback-return': ['error'],
+		'prefer-destructuring': [
+			'error',
+			{
+				array: true,
+				object: true,
+			},
+			{
+				enforceForRenamedProperties: false,
+			},
+		],
+		quotes: [2, 'single', { allowTemplateLiterals: true }],
+		'prefer-template': ['error'],
+		'no-useless-escape': ['error'],
+		'prefer-rest-params': ['error'],
+		'no-new-func': ['error'],
+		'no-param-reassign': ['error'],
+		'prefer-spread': ['error'],
+		'function-paren-newline': ['error'],
+		'prefer-arrow-callback': ['error'],
+		'implicit-arrow-linebreak': ['error'],
+		'no-useless-constructor': ['error'],
+		semi: ['error', 'never'],
+		'eol-last': 0,
+		'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+	},
+	settings: {
+		'import/resolver': {
+			'babel-module': {},
+		},
+	},
+}
