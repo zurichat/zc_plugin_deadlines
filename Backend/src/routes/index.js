8b34da16614ad9d22b9d5d079af31e20f17ae26f
AndrewGlago <andrewglago1@gmail.com>
2021-08-31 01:24:33 +0000

    comment style

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,7 +2,7 @@
     Copyright 2021, Zuri plugin reminder.
     All rights reserved.
     Written By: King Etiosasere, 30th August 2021
-**/
+* */
 
 import { Router } from 'express'
 import userController from '@controllers/user.controller'
--- Backend/src/shared/http/server.js
+++ Backend/src/shared/http/server.js
@@ -2,11 +2,12 @@
     Copyright 2021, Zuri plugin reminder.
     All rights reserved.
     Written By: King Etiosasere, 30th August 2021
-**/
+* */
 
 import express from 'express'
 import dotenv from 'dotenv'
 import routes from '@routes'
+
 dotenv.config()
 
 const app = express()
