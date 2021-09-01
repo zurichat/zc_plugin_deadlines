5e3f1f0b982f1d7422b01f31d5799f3e17068d98
dobbleXg <kennybajomo@gmail.com>
2021-09-01 07:40:42 +0100

    [ErrorClass.js]: Fixes #177 HandleUnUniqueConstraintError

--- src/shared/errors/ErrorClass.js
+++ src/shared/errors/ErrorClass.js
@@ -1,1 +1,24 @@
-// your code goes here
+/**
+ *
+ */
+class UniqueConstraintError extends Error {
+	/**
+	 *
+	 * @param {*} statusCode
+	 * @param {*} message
+	 * @param {*} isOperational
+	 * @param {*} stack
+	 */
+	constructor(statusCode = 409, message, isOperational = true, stack = '') {
+		super(message)
+		this.statusCode = statusCode
+		this.isOperational = isOperational
+		if (stack) {
+			this.stack = stack
+		} else {
+			Error.captureStackTrace(this, this.constructor)
+		}
+	}
+}
+
+export default UniqueConstraintError
