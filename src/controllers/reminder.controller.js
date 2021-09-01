186f1cabd9d3acb19df01ba0711b3c204e6a58b3
ed-knowah <edetnoah@gmail.com>
2021-09-01 19:47:26 +0100

    added get reminders function

--- src/controllers/reminder.controller.js
+++ src/controllers/reminder.controller.js
@@ -3,10 +3,21 @@
     All rights reserved.
     Written By: King Etiosasere, 30th August 2021
 * */
-
 const reminderController = {
 	create: async (req, res) => {
 		return res.send('heelo')
+	},
+
+	getReminders: async (req, res) => {
+		// eslint-disable-next-line consistent-return
+		const reminders = async () => {
+			try {
+				return await axios.get(process.env.DB_URL)
+			} catch (error) {
+				console.error(error)
+			}
+		}
+		return res.send(reminders)
 	},
 }
 
