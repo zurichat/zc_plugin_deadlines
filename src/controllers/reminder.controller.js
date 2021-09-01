a45b467223f10f1d9e46cbb47d38d9f10b786c9b
ed-knowah <edetnoah@gmail.com>
2021-09-01 23:41:40 +0100

    fixed get reminder function

--- src/controllers/reminder.controller.js
+++ src/controllers/reminder.controller.js
@@ -1,7 +1,7 @@
 /**
     Copyright 2021, Zuri plugin reminder.
     All rights reserved.
-    Written By: King Etiosasere, 30th August 2021
+    Written By: King Etiosasere, ed_knowah 30th August 2021
 * */
 const reminderController = {
 	create: async (req, res) => {
@@ -10,14 +10,19 @@ const reminderController = {
 
 	getReminders: async (req, res) => {
 		// eslint-disable-next-line consistent-return
-		const reminders = async () => {
+		const getReminders = async () => {
 			try {
-				return await axios.get(process.env.DB_URL)
+				return await axios.get(process.env.MONGO_DB_URL)
 			} catch (error) {
 				console.error(error)
 			}
 		}
-		return res.send(reminders)
+		const reminders = async () => {
+			const foundReminders = await getReminders()
+			return res.send(foundReminders.data)
+		}
+
+		return reminders()
 	},
 }
 
