0dc56cc2ebe6628351d47cc1ea0abc6e521052e8
Adetoba Adedeji <adetoba22@gmail.com>
2021-09-02 23:37:33 +0100

    Implemented custom tailwind for the styling

--- frontend/src/components/newDeadline/save-deadline-button.jsx
+++ frontend/src/components/newDeadline/save-deadline-button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SaveDeadlineButton = () => {
 	return (
-		<button className=" bg-[#00B87C] hover:bg-[#00B87C] text-[#FFFFFF] p-2 sm:py-2 sm:px-6 rounded">
+		<button className=" bg-brand-primary text-white p-2 sm:py-2 sm:px-6 rounded">
 			Save Deadline
 		</button>
 	)
