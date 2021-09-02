063f02a52986aee02dad9045d31e7ad0039d0a72
Adetoba Adedeji <adetoba22@gmail.com>
2021-09-02 15:11:35 +0100

    [Modal]: Fixes #215 created Save-Deadline button

---
+++ frontend/src/components/newDeadline/save-deadline-button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SaveDeadlineButton = () => {
+	return (
+		<div className="flex items-baseline my-8 text-base">
+			<button className=" bg-green-500 hover:bg-green-600 text-white p-2 sm:py-2 sm:px-6 rounded">
+				Save Deadline
+			</button>
+		</div>
+	)
+}
+
+export default SaveDeadlineButton
