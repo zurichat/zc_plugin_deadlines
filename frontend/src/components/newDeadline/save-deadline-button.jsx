d196f8220fc7101ef72e5c8366eb128e876523b1
Adetoba Adedeji <adetoba22@gmail.com>
2021-09-02 21:37:04 +0100

    Fixed the enclosing div around the button

--- frontend/src/components/newDeadline/save-deadline-button.jsx
+++ frontend/src/components/newDeadline/save-deadline-button.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const SaveDeadlineButton = () => {
 	return (
-		<div className="flex items-baseline my-8 text-base">
-			<button className=" bg-green-500 hover:bg-green-600 text-white p-2 sm:py-2 sm:px-6 rounded">
-				Save Deadline
-			</button>
-		</div>
+		<button className=" bg-[#00B87C] hover:bg-[#00B87C] text-[#FFFFFF] p-2 sm:py-2 sm:px-6 rounded">
+			Save Deadline
+		</button>
 	)
 }
 
