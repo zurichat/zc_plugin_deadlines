b9f2b6f83270893123dd54f77b157bae2a128fc1
Elvis <okaforelvis24@gmail.com>
2021-09-01 23:57:11 -0700

    whiteboard.jsx #252 created whiteboard

--- frontend/src/whiteBoard.jsx
+++ frontend/src/whiteBoard.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 
 const WhiteBoard = () => {
 	return (
-		<div className=" h-screen w-screen border-2 mt-1 ml-1 mr-1 box-border flex max-h-screen max-w-screen md:mt-20 md:ml-72">
+		<div className=" h-screen w-screen border-1 mt-1 ml-1 mr-1 box-border flex max-h-screen max-w-screen md:mt-1 md:ml-1">
 			{' '}
 		</div>
 	)
