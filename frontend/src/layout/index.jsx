a8a2874e1b4af474c827be19676f0ce8a923c981
Sophia Ebere-Shepherd <sofiebereshepherd@gmail.com>
2021-09-02 18:12:34 +0100

    Whiteboard
    The white board displays

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -7,11 +7,11 @@ import DeadlineStatus from '@deadlineStatus'
 
 const Layout = () => {
 	return (
-		<>
+		<div className="flex flex-col bg-white w-9/12 h-auto py-4 my-4 mr-2 gap-4 shadow border-2 border-gray-200 rounded">
 			<Nav />
 			<DeadlineList />
 			<DeadlineStatus />
-		</>
+		</div>
 	)
 }
 
