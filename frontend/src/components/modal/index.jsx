0445d32a9d85993645c9d8604c1f1f661eb6595a
jutivia <rukkylaw.tl@gmail.com>
2021-09-02 15:53:22 +0100

    [index]:fixes #281 Add Re-useable Modal Dialog

--- frontend/src/components/modal/index.jsx
+++ frontend/src/components/modal/index.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Dialog, Transition} from '@headlessui/react'
 
 const Modal = () => {
-	return (
-		<>
+	let [isOpen, setIsOpen]=  useState(true)
+	const openModal =()=>setIsOpen(true)
+	const closeModal =()=>setIsOpen(false)
+	return (<>
+		<Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
 			<div>Reusable Modal</div>
 		</>
 	)
