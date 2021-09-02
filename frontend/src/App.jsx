0e60c7fe57836435ecf5b07b9f4336dfc9f8436e
demiladeore <oluwafemidemi47@gmail.com>
2021-09-02 18:29:22 +0100

    [timer-text-input]: #222 Add timer text input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import TimerTextBox from './components/newDeadline/timer-text-box'
 
 const App = () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen text-center ">
-			<header className="text-3xl text-cra-primary font-craFont">
+			{/* <header className="text-3xl text-cra-primary font-craFont">
 				<h1>Zuri Chat Reminder Plugin</h1>
 				<p>
 					Reminder plugin for the open-source{' '}
@@ -11,7 +12,8 @@ const App = () => {
 						Zuri Chat
 					</a>
 				</p>
-			</header>
+			</header> */}
+			<TimerTextBox />
 		</div>
 	)
 }
