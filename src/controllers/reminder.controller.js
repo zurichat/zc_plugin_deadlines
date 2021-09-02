10b2cbeac1b4709070539752c68eb1c67227aae3
Adeshina Ibrahim <debowale01@gmail.com>
2021-09-02 10:30:23 +0100

    [setDeadline]: fixes #228 added setDeadline function

--- src/controllers/reminder.controller.js
+++ src/controllers/reminder.controller.js
@@ -4,9 +4,46 @@
     Written By: King Etiosasere, 30th August 2021
 * */
 
+import axios from 'axios'
+
 const reminderController = {
 	create: async (req, res) => {
 		return res.send('heelo')
+	},
+	setDeadline: async (req, res) => {
+		// get inputs from the request body
+		const {
+			teamsAssigned,
+			membersAssigned,
+			title,
+			description,
+			startDates,
+			dueDates,
+		} = req.body
+		// post data to be sent
+		const postData = {
+			teamsAssigned,
+			membersAssigned,
+			title,
+			description,
+			startDates,
+			dueDates,
+		}
+		// axios config
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				token: 'Token For Authentication Here',
+			},
+		}
+		try {
+			await axios.post(process.env.MONGO_DB_URL, postData, config)
+		} catch (error) {
+			console.error(error)
+		}
+		return res
+			.status(201)
+			.json({ status: 'success', message: 'Deadline successfully set' })
 	},
 }
 
