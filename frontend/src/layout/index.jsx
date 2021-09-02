2c0465cb2821319d8f845308fbae043881465c0e
Sophia Ebere-Shepherd <sofiebereshepherd@gmail.com>
2021-09-02 18:40:53 +0100

    Main Page Layout
    Main Page is divided into two major containers

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -9,8 +9,15 @@ const Layout = () => {
 	return (
 		<div className="flex flex-col bg-white w-9/12 h-auto py-4 my-4 mr-2 gap-4 shadow border-2 border-gray-200 rounded">
 			<Nav />
-			<DeadlineList />
-			<DeadlineStatus />
+
+			<div className="grid grid-cols-3">
+				<div className="col-span-2 p-6 m-8 h-screen border-r-2 border-gray-300">
+					<DeadlineList />
+				</div>
+				<div className="col-span-1 p-6 h-5/6 bg-gray-50 mr-4 my-8 rounded ">
+					<DeadlineStatus />
+				</div>
+			</div>
 		</div>
 	)
 }
