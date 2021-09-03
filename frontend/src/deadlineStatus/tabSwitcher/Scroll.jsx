f3e69acacd487d525cb979b72c6b9a79b668ba5b
Halimah Ogunbiyi <88124618+Kenkeluke@users.noreply.github.com>
2021-09-03 21:25:09 +0100

    Added a scroll file that addresses the infinite scrollbar for now

---
+++ frontend/src/deadlineStatus/tabSwitcher/Scroll.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Scroll = () => {
+	return (
+		<div className="rounded border-solid border mt-5 overflow-y-scroll overscroll-auto">
+			{' '}
+			<p>
+				{' '}
+				This is a scrollable deadline section Deadline cards will feature here{' '}
+				<br />
+				and will be scrollable without affecting the whole page <br />
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Possimus
+				ratione ipsam sapiente omnis quas sint, porro deleniti <br /> velit quis
+				veniam fugiat amet optio, repellat reprehenderit <br /> molestiae,
+				provident totam blanditiis? Reiciendis placeat <br /> laborum nesciunt
+				pariatur sit temporibus debitis impedit <br />
+				assumenda aliquid. Laudantium optio fugiat voluptate commodi?
+			</p>
+		</div>
+	)
+}
+export default Scroll
