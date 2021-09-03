7f8c0268ade06c8fa7bbe969252e4a39b5775cd0
mac <mac@macs-MacBook-Pro.local>
2021-09-03 08:37:53 +0100

    deadline button created a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const App = () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen text-center ">
@@ -12,6 +11,9 @@ const App = () => {
 					</a>
 				</p>
 			</header>
+			<button className="w-30 bg-green-500 hover:bg-blue-500 text-white py-2 px-4 rounded">
+				Set Deadline
+			</button>
 		</div>
 	)
 }
