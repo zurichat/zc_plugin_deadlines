5d0a16145b168f11804fc979b3da96b367c7d156
Haastrup Elijah <haastrupea@gmail.com>
2021-09-03 23:28:02 +0100

    [fix bad gate way] fix to badgateway

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,9 +14,8 @@ router.get('/ping', (req, res) =>
 	res.json({ message: 'Hello! You have found the zc_plugin_reminder api' })
 )
 
-
 router.post('/reminders', reminderValidation, reminderController.create)
 router.route('/getReminders').get(reminderController.findAll)
-router.route('/reminders').get(reminderController.getAll)
+router.get('/reminders', reminderController.getAll)
 
 export default router
