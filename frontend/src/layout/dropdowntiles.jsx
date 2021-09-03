2f842d89918f1ff7b1f1ea934553503e27d81f55
Tokonigeorge <65655487+Tokonigeorge@users.noreply.github.com>
2021-09-03 22:36:40 +0100

    [dropdowntiles] : Fixes #249 Add sortbar dropdown tiles.

---
+++ frontend/src/layout/dropdowntiles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dropdowntiles = ({sort_tag}) => {
+    return (
+        <p className='p-1 border-b border-black'>{sort_tag}</p>
+    )
+}
+
+export default Dropdowntiles
