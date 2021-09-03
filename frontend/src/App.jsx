3730de76d7f1c9fbfcae78b4e4bd0b822e4c5254
spykelion <ndilionel6@gmail.com>
2021-09-03 09:10:30 +0100

    [Cancel-Button]: Fixes issue #218 view cancel button for task popup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import CancelButton from './deadlineList/components/CancelButton'
 
 const App = () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen text-center ">
 			<header className="text-3xl text-cra-primary font-craFont">
-				<CancelButton />
 				<h1>Zuri Chat Reminder Plugin</h1>
 				<p>
 					Reminder plugin for the open-source{' '}
