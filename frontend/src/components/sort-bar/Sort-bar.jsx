c66bda06dc951f7ef8971c825240784b1d70fc4d
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2021-09-03 12:58:15 +0100

    Add files via upload

---
+++ frontend/src/components/sort-bar/Sort-bar.jsx
@@ -0,0 +1,16 @@
+
+const  dropdown =() => {
+    return (
+      <div className="dropdown">
+          <form action="">
+            <select name="box" id="list" > <i class="arrow down"></i>
+                        <option value="" selected> date</option>
+                            <option value="">time</option>
+                          
+                </select>
+                </form>
+      </div> 
+    );
+  }
+  
+  export default dropdown;
