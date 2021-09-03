e086a08b6b116bc60503c19969ffadfc738f0e9a
Jutivia <rukkylaw.tl@gmail.com>
2021-09-03 18:00:34 +0100

    editing app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Modal from './components/modal/index'
+
 const App = () => {
 	return (
 		<>
-			{/* <div className="flex flex-col items-center justify-center h-screen text-center"> */}
-			<Modal />
-			{/* </div> */}
+			<div className="flex flex-col items-center justify-center h-screen text-center">
+			App
+			</div>
 		</>
 	)
 }
