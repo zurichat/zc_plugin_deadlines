157d293fb8199c534e9a1b02369bd032554cc8f3
spykelion <ndilionel6@gmail.com>
2021-09-03 09:00:06 +0100

    [Cancel-Button]: Fixes issue #218 view cancel button for task popup

---
+++ frontend/src/deadlineList/components/CancelButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CancelButton = () => {
+	return (
+		<>
+			<div>
+				<button className="bg-red-500 text-white px-2 py-4 hover:bg-red-700 rounded">Cancel </button>
+			</div>
+		</>
+	)
+}
+
+export default CancelButton
