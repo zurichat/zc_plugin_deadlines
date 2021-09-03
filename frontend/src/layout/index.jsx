743ad18bb3b23d5b4b58893ad10b2bf722019d7c
AndrewGlago <andrewglago1@gmail.com>
2021-09-03 15:17:19 +0000

    update grid sizing

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -5,14 +5,13 @@ import DeadlineStatus from '../deadlineStatus'
 
 const Layout = () => {
 	return (
-		<div className="flex flex-col bg-white w-9/12 h-auto py-4 my-4 mr-2 gap-4 shadow border-2 border-gray-200 rounded">
+		<div className="flex flex-col w-full h-screen gap-4 p-1">
 			<Nav />
-
 			<div className="grid grid-cols-3">
-				<div className="col-span-2 p-6 m-8 h-screen border-r-2 border-gray-300">
+				<div className="col-span-2 p-6 m-8">
 					<DeadlineList />
 				</div>
-				<div className="col-span-1 p-6 h-5/6 bg-gray-50 mr-4 my-8 rounded ">
+				<div className="col-span-1 p-6 h-5/6 mr-4 my-8">
 					<DeadlineStatus />
 				</div>
 			</div>
