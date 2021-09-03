f9142c4368e3905ae6bfd3dddf546bedb9378c41
Sophia Ebere-Shepherd <sofiebereshepherd@gmail.com>
2021-09-04 00:10:28 +0100

    Fixes for #195 and #289
    changes implemented as advised

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
 		<div className="bg-white w-full h-screen gap-4 p-1">
 			<Nav />
 			<div className="grid lg:grid-cols-3">
-				<div className="lg:col-span-2 h-screen border-r-2 p-6">
+				<div className="lg:col-span-2 h-screen border-r-2 border-opacity-40 p-6">
 					<DeadlineList />
 				</div>
 				<div className="lg:col-span-1 p-6 h-screen bg-gray-50 opacity-50 m-4">
