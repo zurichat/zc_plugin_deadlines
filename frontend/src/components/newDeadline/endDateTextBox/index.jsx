3fadf3ea3679e0acfb2741b6ec3b10a19b15374f
Austinet <austinetsplendour@gmail.com>
2021-09-04 12:21:02 +0100

    removed the form and div tags

--- frontend/src/components/newDeadline/endDateTextBox/index.jsx
+++ frontend/src/components/newDeadline/endDateTextBox/index.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 
 const EndDateTextBox = () => {
 	return (
-		<div className="EndDateTextBox">
-			<form action="#">
-				<input
-					type="text"
-					name="startDate"
-					id="startDate"
-					placeholder="DD   /    MM     /   YY"
-					className="px-3 py-2 bg-gray-200 border-none outline-none text-center border-box rounded"
-				/>
-			</form>
-		</div>
+		<input
+			type="text"
+			name="startDate"
+			id="startDate"
+			placeholder="DD   /    MM     /   YY"
+			className="px-3 py-2 bg-gray-200 border-none outline-none text-center border-box rounded"
+		/>
 	)
 }
 export default EndDateTextBox
