fb2c696a054bafbee548d531f52b970109290dc3
Faith Oladele <faitholadele31@gmail.com>
2021-09-04 11:25:22 -0700

    [Text Description]: Fixes #193 Add text-area

--- frontend/src/deadlineList/TextDescription.jsx
+++ frontend/src/deadlineList/TextDescription.jsx
@@ -3,10 +3,17 @@ import React from 'react'
 const TextDescription = () => {
 	return (
 		<>
-			<div>This task will be a sprint,</div>
-			<div>we are to use the design...</div>
+			<div>This task will be a sprint, we are to use the design</div>
 		</>
 	)
 }
+function truncateString(string, limit) {
+	if (string.length > limit) {
+		return string.substring(0, limit) + "..."
+	} else {
+		return string
+	}
+}
+truncateString("This task will be a sprint, we are to use the design", 52)
 
 export default TextDescription
