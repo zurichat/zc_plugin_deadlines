a6616cc35a709cde9a9eadf6276cb7bb6d06d776
olatunbosun-iyare <olat2d@yahoo.com>
2021-09-04 02:33:36 +0100

    Closes #194 View Text Input for the Assignee

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
+import AssigneeInput from './AssigneeInput'
 
 const App = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen text-center ">
-			<header className="text-3xl text-cra-primary font-craFont">
-				<h1>Zuri Chat Reminder Plugin</h1>
-				<p>
-					Reminder plugin for the open-source{' '}
-					<a href="https://zuri.chat" className="text-cra-link">
-						Zuri Chat
-					</a>
-				</p>
-			</header>
+		<div>
+			<AssigneeInput />
 		</div>
 	)
 }
