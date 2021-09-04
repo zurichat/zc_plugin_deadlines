a57790839839124441292a47b9722c4ccfc6faec
Tokonigeorge <65655487+Tokonigeorge@users.noreply.github.com>
2021-09-04 10:46:07 +0100

    [searchbar] ; fixes #199 View search bar.

---
+++ frontend/src/layout/searchbar.jsx
@@ -0,0 +1,33 @@
+import {React, useState} from 'react'
+
+import { SearchIcon } from '@heroicons/react/solid'
+
+const Searchbar = ({...props}) => {
+	const [input, setInput] = useState('')
+    
+	const handleInput = (e) => {
+    setInput(e.target.value);
+	}
+
+    const handleEnterSubmit = (e) =>{
+     if(e.key === 'Enter') {handleSubmit(e)}
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setInput('')
+    }
+
+	return (
+		<div className="shadow appearance-none border rounded w-1/6 py-2 px-5 inline-flex justify-between items-center">
+			<label for="task search" role="search">
+			<input
+				className="text-gray-400 leading-tight focus:outline-none focus:shadow-outline text-sm"
+				placeholder="Search" type="text" value={input} onChange={handleInput} id="task search" onKeyDown={handleEnterSubmit}
+			/>
+			</label>
+          < SearchIcon className="text-gray-400 w-3.5" type="submit" onClick={handleSubmit}/>
+		</div>
+	)
+}
+export default Searchbar
