eb11c6b6ed8f235c7a0da98f3584a1d46ce96237
Elvis <okaforelvis24@gmail.com>
2021-09-04 07:30:10 -0700

    [Sort Bar]:Fixes #356 View Sortbar mobile

--- frontend/src/layout/nav/index.jsx
+++ frontend/src/layout/nav/index.jsx
@@ -5,13 +5,15 @@ import Searchbar from './searchbar'
 const Nav = () => {
 	return (
 		<nav className="px-5">
-			<div className="flex justify-between py-5 w-4/6">
-				<p className="font-bold text-xl">Deadlines</p>
+			<div className="flex justify-between py-6 w-4/6">
+				<p className="font-bold mt-2 transform translate-y-6 md:translate-x-0 md:translate-y-0 text-xl">
+					Deadlines
+				</p>
 				<div className="flex">
-					<div className="mx-5">
+					<div className="mx-5 transform -translate-y-6 -translate-x-1/4 md:translate-x-0 md:translate-y-0 md:mx-5">
 						<Searchbar />
 					</div>
-					<div className="mx-5 flex-grow-0">
+					<div className="mx-5 w-max transform translate-y-6 transform -translate-x-3/4 md:translate-x-0 md:translate-y-0 flex-grow-0">
 						<Sortbar />
 					</div>
 				</div>
