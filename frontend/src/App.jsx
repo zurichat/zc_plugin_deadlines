eba78d7dc4ae61caf8097d6b78e2365abc816223
AndrewGlago <andrewglago1@gmail.com>
2021-09-04 18:04:01 +0000

    view Modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ViewDeadline from './components/viewDeadline'
 import Layout from './layout'
 
 const App = () => {
-	return <Layout />
+	return (
+		<>
+			<ViewDeadline />
+			<Layout />
+		</>
+	)
 }
 
 export default App
--- frontend/src/components/viewDeadline/index.jsx
+++ frontend/src/components/viewDeadline/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import Modal from '../modal'
+
+import ViewDescription from './components/ViewDescription'
+import ViewTime from './components/ViewTime'
+import ViewTitle from './components/ViewTitle'
+import ViewAssigned from './components/ViewAssigned'
+import ViewDueDate from './components/ViewDueDate'
 
 const ViewDeadline = () => {
 	return (
-		<>
-			<div>View Deadline Modal</div>
-		</>
+		<Modal open={true} title={'View Deadline'}>
+			<div className="flex flex-row">
+				<div className="text-left">
+					<ViewTitle title={'Designing Zuri Dashboard'} />
+					<ViewDescription
+						description={
+							'This task will be a sprint, we are to use the design to implement a responsive dashboard'
+						}
+					/>
+				</div>
+				<div id="divider" className="border-l mx-5" />
+				<div>
+					<ViewDueDate dueDate={new Date()} />
+					<ViewTime time={'3:00 PM'} />
+					<ViewAssigned />
+				</div>
+			</div>
+		</Modal>
 	)
 }
 
