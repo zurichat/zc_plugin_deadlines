f1921cb43967fc740dccbd185419541349bb7b20
olatunbosun-iyare <olat2d@yahoo.com>
2021-09-04 13:41:14 +0100

    Fixes #194 Restores the App.jsx original format and renders the view text input for the assignee with the dropdown placeholder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,19 @@ import AssigneeInput from './AssigneeInput'
 
 const App = () => {
 	return (
-		<div>
-			<AssigneeInput />
+		<div className="flex flex-col items-center justify-center h-screen text-center ">
+			<header className="text-3xl text-cra-primary font-craFont">
+				<h1>Zuri Chat Reminder Plugin</h1>
+				<p>
+					Reminder plugin for the open-source{' '}
+					<a href="https://zuri.chat" className="text-cra-link">
+						Zuri Chat
+					</a>
+				</p>
+			</header>
+			<div className="text-left pt-7">
+				<AssigneeInput />
+			</div>
 		</div>
 	)
 }
