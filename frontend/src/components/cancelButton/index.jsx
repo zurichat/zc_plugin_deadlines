0e810d7c95db6e8e0a280ea25cdd8f0633ad1aee
spykelion <ndilionel6@gmail.com>
2021-09-04 13:07:41 +0100

    [Cancel-Button] Fixes #218 view cancel button on task reminder

--- frontend/src/components/cancelButton/index.jsx
+++ frontend/src/components/cancelButton/index.jsx
@@ -9,7 +9,7 @@ import * as Icons from 'heroicons-react'
 const CancelButton = ({ ...props }) => {
 	return (
 		<button {...props}>
-			<Icons.XOutline className="bg-grey-500 text-white p-2 hover:bg-grey-600 rounded-full flex items-center justify-center w-15 h-15" />
+			<Icons.XOutline className="bg-grey-500 text-white p-2 hover:bg-grey-600 rounded-full flex items-center justify-center w-15 h-15 " />
 		</button>
 	)
 }
