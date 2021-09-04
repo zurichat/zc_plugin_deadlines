d4a02b5c64280e87439d84419132d58c66e34614
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-09-04 15:43:45 +0100

    Fixed test for deleteOne Function

--- tests/deleteReminder.spec.js
+++ tests/deleteReminder.spec.js
@@ -4,10 +4,9 @@
     Written By: King Etiosasere, 30th August 2021 
 */
 import app from '@shared/http/server'
-import { expect } from 'chai'
-import { request } from 'supertest'
+import request from 'supertest'
 
-describe('DELETE /api/v1/reminders/id', () => {
+describe('DELETE /api/v1/reminders/:id', () => {
 	it('reminder successfully deleted', (done) => {
 		request(app).delete('/api/v1/reminders/:id').expect(200, done)
 	})
