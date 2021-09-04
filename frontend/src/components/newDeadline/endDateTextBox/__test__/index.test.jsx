4bec9db37a45921dc22d3f7509089035cfb8ee43
Austinet <austinetsplendour@gmail.com>
2021-09-04 13:10:15 +0100

    Edited the filename as instructed

--- frontend/src/components/newDeadline/endDateTextBox/__test__/index.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-
-import EndDateTextBox from '../'
-
-test('<EndDateTextBox /> renders without crashing', () => {
-	render(<EndDateTextBox />)
-})
