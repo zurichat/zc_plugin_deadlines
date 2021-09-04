a72370db35031c44734bc88d8dae5282263fab0e
temscoco <otemilade84@gmail.com>
2021-09-04 22:40:39 +0100

    implemented changes in the textarea component

--- frontend/src/components/newDeadline/text-area.jsx
+++ frontend/src/components/newDeadline/text-area.jsx
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-class App extends Component {
-    componentDidMount (){
-        this.textarea.focus();
-    }
-    render(){
-        const style = {
-            minHeight: '5.6rem',
-            minWidth: '15.3rem',
-        };
-
-        return (
-            <div> Textarea <br />
-                <label htmlFor="" className="font-sans font-bold text-2x1">Description</label>
-                <textarea
-                className="max-w-lg max-h-44 p-4 resize-none box-border" 
-                style={style}
-                ref={c=>this.textarea=c}
-                placeholder="Type a message"
-                rows={1} defaultValue=""/>
-            </div>
-        );
-    }
+const TextArea = ({ value, onChange }) => {
+	return (
+		<div>
+			<label className="text-sm font-bold mb-1 block text-left">
+				Description
+			</label>
+			<textarea
+				value={value}
+				onChange={onChange}
+				className="w-full max-h-44 p-4 text-base block bg-lightGrey"
+			/>
+		</div>
+	)
 }
 
+export default TextArea
