cba6342251bdb155ccce7ca176345c7432194f0c
Abuabdurrahman <abdulbasitdamilola6@gmail.com>
2021-09-04 04:51:40 +0100

    changes implemented

--- frontend/src/layout/sortBar/index.jsx
+++ frontend/src/layout/sortBar/index.jsx
@@ -12,22 +12,20 @@ function MenuItem(props) {
 }
 const SortBar = () => {
 	return (
-		<div className="w-56 text-right fixed top-16 right-10">
-			<Menu as="div" className="relative inline-block text-left">
-				<div>
-					<Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-						Sort
-					</Menu.Button>
-				</div>
-				<Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-					<div className="px-4 py-1 ">
-						<MenuItem item="Newest" />
-						<MenuItem item="Oldest" />
-						<MenuItem item="hahaah" />
-					</div>
-				</Menu.Items>
-			</Menu>
-		</div>
+		<Menu as="div" className="w-36 inline-block text-left">
+			<Menu.Button
+				as="input"
+				className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+				type="button"
+				value="Sort"
+			/>
+			<Menu.Items className="w-56 mt-2 bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+				<MenuItem item="Date ascending" />
+				<MenuItem item="Date descending" />
+				<MenuItem item="Time ascending" />
+				<MenuItem item="Time descending" />
+			</Menu.Items>
+		</Menu>
 	)
 }
 
