e2b36266b60e01f37f64bcda96f07b624b02a790
Faith Oladele <faitholadele31@gmail.com>
2021-09-04 00:36:40 -0700

    [TextDescription]: Fixes #193 Add text-area

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import TextDescription from './deadlineList/TextDescription'
 
 const App = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen text-center">
-			App
+		<div className="flex flex-col items-center justify-center h-screen text-center text-gray-300 leading-3 font-sans">
+			<TextDescription></TextDescription>
 		</div>
 	)
 }
---
+++ frontend/src/deadlineList/TextDescription.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TextDescription = () => {
+	return (
+		<>
+			<div>This task will be a sprint,</div>
+			<div>we are to use the design...</div>
+		</>
+	)
+}
+
+export default TextDescription
