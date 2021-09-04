a74dae4c1d4c48b954ad550f4fd58df163045cfb
AndrewGlago <andrewglago1@gmail.com>
2021-09-04 01:48:31 +0000

    remove placeholders

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -3,31 +3,13 @@ import Nav from './nav'
 import DeadlineList from '../deadlineList'
 import DeadlineStatus from '../deadlineStatus'
 
-// Temporary placeholder element for DeadlineList function
-const Title = () => {
-	return <div>Zuri Project</div>
-}
-
-// Temporary placeholder array for DeadlineList function
-const list = [
-	{
-		title: <Title />,
-	},
-	{
-		title: <Title />,
-	},
-	{
-		title: <Title />,
-	},
-]
-
 const Layout = () => {
 	return (
 		<div className="flex flex-col w-full h-screen gap-4 p-1">
 			<Nav />
 			<div className="grid grid-cols-3">
 				<div className="col-span-2 p-6 m-8">
-					<DeadlineList list={list} />
+					<DeadlineList />
 				</div>
 				<div className="col-span-1 p-6 h-5/6 mr-4 my-8">
 					<DeadlineStatus />
