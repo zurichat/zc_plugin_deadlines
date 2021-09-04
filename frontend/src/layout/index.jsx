a619f1164ccc0ba13cf306e5c71016c631968d03
AndrewGlago <andrewglago1@gmail.com>
2021-09-04 02:41:36 +0000

    mobile responsiveness

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -7,11 +7,11 @@ const Layout = () => {
 	return (
 		<div className="bg-white w-full h-screen gap-4 p-1">
 			<Nav />
-			<div className="grid lg:grid-cols-3">
-				<div className="lg:col-span-2 h-screen border-r-2 border-opacity-40 p-6">
+			<div className="flex flex-col md:grid md:grid-cols-3 ">
+				<div className="md:col-span-2 h-screen/1.5 md:h-screen overflow-y-scroll border-r-2 border-opacity-40 p-6">
 					<DeadlineList />
 				</div>
-				<div className="lg:col-span-1 p-6 h-screen bg-gray-50 opacity-50 m-4">
+				<div className="md:col-span-1 p-6 md:h-screen m-4">
 					<DeadlineStatus />
 				</div>
 			</div>
