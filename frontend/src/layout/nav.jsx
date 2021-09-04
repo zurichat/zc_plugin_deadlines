f7ddf648130b19740f12006f25418874ed16ea63
Meenarh <oahmedaminat@gmail.com>
2021-09-04 12:36:39 +0100

    [Deadline text]: Fixes #246 View Deadline Text'

--- frontend/src/layout/nav.jsx
+++ frontend/src/layout/nav.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import SetDeadline from './Button.jsx'
 
 const Nav = () => {
 	return (
 		<nav>
-			<div className="static w-24 h-6 bg-green-600 font-semibold text-lg leading-7 text-white flex-none order-none flex-grow-0">
-				<SetDeadline> Set Deadlines</SetDeadline>
-			</div>
+			<p>Nav</p>
 		</nav>
 	)
 }
