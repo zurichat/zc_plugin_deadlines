e852a3afe1bbd5747bda7ecd88f2ea96d115519d
AndrewGlago <andrewglago1@gmail.com>
2021-09-04 16:03:57 +0000

    update fxn declaration

--- frontend/src/components/newDeadline/AssigneeInput.jsx
+++ frontend/src/components/newDeadline/AssigneeInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function AssigneeInput() {
+const AssigneeInput = () => {
 	return (
 		<form className="w-full max-w-sm">
-			<label className="font-weight:700 text-align: left;">Assigne to</label>
+			<label className="font-weight:700 text-align: left;">Assign to</label>
 			<div className="flex items-center border-b border-black-500 py-2">
 				<input
 					className="bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
