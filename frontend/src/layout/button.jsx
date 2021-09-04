d5e8af8de1104c653601626dca574af129d08d31
Meenarh <oahmedaminat@gmail.com>
2021-09-04 12:08:50 +0100

    [Deadline text]: Fixes #246 View Deadline Text"

---
+++ frontend/src/layout/button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const setDeadline = (props) => {
+	return <button> {props.children}</button>
+}
+
+export default setDeadline
--- frontend/src/layout/nav.jsx
+++ frontend/src/layout/nav.jsx
@@ -1,24 +1,14 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-useless-constructor */
 import React from 'react'
+import SetDeadline from './Button.jsx'
 
 const Nav = () => {
 	return (
 		<nav>
-			<p>Nav</p>
+			<div className="static w-24 h-6 bg-green-600 not-italic font-semibold text-lg leading-7 text-white flex-none order-none flex-grow-0">
+				<SetDeadline> Set Deadlines</SetDeadline>
+			</div>
 		</nav>
 	)
-}
-
-// eslint-disable-next-line unused-imports/no-unused-vars
-class DeadlineText extends React.Component {
-	constructor(props) {
-		super(props)
-	}
-
-	render() {
-		return <Nav />
-	}
 }
 
 export default Nav
