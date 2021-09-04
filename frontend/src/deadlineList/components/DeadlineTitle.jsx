f6f7dd6e2e33d4d8683ed6bc1cbba25177fc785c
AndrewGlago <andrewglago1@gmail.com>
2021-09-04 14:41:31 +0000

    update

--- frontend/src/deadlineList/components/DeadlineTitle.jsx
+++ frontend/src/deadlineList/components/DeadlineTitle.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-const Title = (props) => {
-	const { title } = props
+const DeadlineTitle = ({ title }) => {
 	return <h1 className="font-bold">{title}</h1>
 }
 
-export default Title
+export default DeadlineTitle
