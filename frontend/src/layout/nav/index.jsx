611169b8d3ebbadcd7b3123764d943bddf7b4c56
Precious Abubakar <misspee007@gmail.com>
2021-09-04 18:21:17 +0100

    [mobile-navbar]: Fixed responsiveness of the navbar

--- frontend/src/layout/nav/index.jsx
+++ frontend/src/layout/nav/index.jsx
@@ -5,13 +5,13 @@ import Searchbar from './searchbar'
 const Nav = () => {
 	return (
 		<nav className="px-5">
-			<div className="flex justify-between py-5 w-4/6">
+			<div className="flex justify-between py-5 md:w-4/6">
 				<p className="font-bold text-xl">Deadlines</p>
 				<div className="flex">
-					<div className="mx-5">
+					<div className="hidden mx-5 md:block">
 						<Searchbar />
 					</div>
-					<div className="mx-5 flex-grow-0">
+					<div className="md:mx-5 md:flex-grow-0">
 						<Sortbar />
 					</div>
 				</div>
