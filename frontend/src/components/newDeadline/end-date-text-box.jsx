4163ed7736834d9b9140edf602d5bbfafb2414fd
Austinet <austinetsplendour@gmail.com>
2021-09-04 15:45:19 +0100

    changed into a date picker

--- frontend/src/components/newDeadline/end-date-text-box.jsx
+++ frontend/src/components/newDeadline/end-date-text-box.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const EndDateTextBox = () => {
 	return (
 		<input
-			type="text"
+			type="date"
 			name="startDate"
 			id="startDate"
 			placeholder="DD   /    MM     /   YY"
