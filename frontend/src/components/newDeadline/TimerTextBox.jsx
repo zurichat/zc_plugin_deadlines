585769f8570a0a356878494a5d449e0715e76614
demiladeore <oluwafemidemi47@gmail.com>
2021-09-04 11:14:57 +0100

    [timer-text-box]: removed AM/PM select option

--- frontend/src/components/newDeadline/TimerTextBox.jsx
+++ frontend/src/components/newDeadline/TimerTextBox.jsx
@@ -6,18 +6,9 @@ const TimerTextBox = () => {
 			<input
 				className="w-1/2 bg-brand-bg-lightGrey rounded border-2 px-5 m-2 h-11 text-brand-text-lightIcon"
 				type="time"
-				value="00:00"
+				defaultValue="00:00"
 				// placeHolder="00:00"
 			/>
-			<select
-				className="p-2 bg-brand-bg-lightGrey px-3 rounded border-2 appearance-none m-2 text-brand-text-lightIcon"
-				name="time"
-			>
-				<option value="AM" selected>
-					AM
-				</option>
-				<option value="PM">PM</option>
-			</select>
 		</div>
 	)
 }
