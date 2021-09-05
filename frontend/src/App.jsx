f272df09db50690076a69f7647dbd96118273e04
demiladeore <oluwafemidemi47@gmail.com>
2021-09-05 09:44:14 +0100

    [reminder-toggle: Fixes issue #221 ran yarn test]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import React, { useContext } from 'react'
+// import React, { useContext } from 'react'
+import React from 'react'
 
-import Layout from './layout'
-import ViewDeadline from './components/viewDeadline'
-import { ModalContext } from './context/ModalContext.jsx'
+// import Layout from './layout'
+// import ViewDeadline from './components/viewDeadline'
+// import { ModalContext } from './context/ModalContext.jsx'
+import ReminderToggle from './components/newDeadline/components/reminder-toggle'
 
 const App = () => {
-	const { modalData } = useContext(ModalContext || {})
+	// const { modalData } = useContext(ModalContext || {})
 
 	return (
-		<>
-			{modalData.modalShow ? <ViewDeadline modalData={modalData} /> : null}
-			<Layout />
-		</>
+		// <>
+		// 	{modalData.modalShow ? <ViewDeadline modalData={modalData} /> : null}
+		// 	<Layout />
+		// </>
+		<ReminderToggle />
 	)
 }
 
--- frontend/src/components/newDeadline/index.jsx
+++ frontend/src/components/newDeadline/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import TimerTextBox from './TimerTextBox'
 
 const NewDeadline = () => {
 	return (
 		<>
 			<div>New Deadline Modal</div>
-			<TimerTextBox />
 		</>
 	)
 }
