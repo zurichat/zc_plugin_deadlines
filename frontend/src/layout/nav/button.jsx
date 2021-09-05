4582fcb74b727b983b0b30c7ef51a430e3f1e033
jutivia <76624942+jutivia@users.noreply.github.com>
2021-09-05 06:15:58 +0100

    Update button.jsx

--- frontend/src/layout/nav/button.jsx
+++ frontend/src/layout/nav/button.jsx
@@ -3,15 +3,14 @@ import NewDeadline from "../../components/newDeadline/index"
 
 const Button = () => {
 const [isDeadline, setIsDeadline]= useState(false)
-	return (
+	return (<>
 		<button
 			className="w-30 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
-			onClick={() => {
-				setModalData({ ...modalData, modalShow: true })
-			}}
-		>
+			onClick={() => setIsDeadline(true)} >
 			Set Deadline
 		</button>
+{isDeadline && <NewDeadline/>}
+</>
 	)
 }
 
