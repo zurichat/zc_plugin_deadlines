891e956f5dd80d8da0aa5c36684adc5e9fe6169c
jutivia <76624942+jutivia@users.noreply.github.com>
2021-09-05 09:44:50 +0100

    Update button.jsx

--- frontend/src/layout/nav/button.jsx
+++ frontend/src/layout/nav/button.jsx
@@ -1,12 +1,12 @@
 import React,{useContext, useState} from 'react'
 import NewDeadline from "../../components/newDeadline/index"
-
+import ModalContext from "../../context/modalContext'
 const Button = () => {
-const [isDeadline, setIsDeadline]= useState(false)
+const {showModal, setShowModal}= ModalContext()
 	return (<>
 		<button
 			className="w-30 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
-			onClick={() => setIsDeadline(true)} >
+			onClick={() => setShowModal(true)} >
 			Set Deadline
 		</button>
 {isDeadline && <NewDeadline/>}
