1b640b74532dc77b6f823f43b5fd2a95d1ad7ea2
AndrewGlago <andrewglago1@gmail.com>
2021-09-05 09:07:21 +0000

    fix text color

--- frontend/src/deadlineStatus/components/date/DueDate.jsx
+++ frontend/src/deadlineStatus/components/date/DueDate.jsx
@@ -10,8 +10,7 @@ const DueDate = ({ tab }) => {
 		date: '31st Aug, 2021',
 		icon: <img src={CalenderIcon} className="h-full" alt="calender-icon" />,
 		iconColor: 'bg-brand-avatar-blue',
-		// Don't judge, I was/am tired when I wrote this
-		actionColor: tab === 'overdue' ? 'brand-accent' : null,
+		tab,
 	}
 	return <Date {...props} />
 }
