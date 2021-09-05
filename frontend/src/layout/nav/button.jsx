c2a07707a0551fea72124325133e871df1836857
AndrewGlago <andrewglago1@gmail.com>
2021-09-05 11:43:56 +0000

    modal show

--- frontend/src/layout/nav/button.jsx
+++ frontend/src/layout/nav/button.jsx
@@ -1,15 +1,23 @@
-import React,{useContext, useState} from 'react'
-import NewDeadline from "../../components/newDeadline/index"
-import {ModalContext} from "../../context/ModalContext'
+import React, { useContext } from 'react'
+import { ModalContext } from '../../context/ModalContext'
+
 const Button = () => {
-const {showModal, setShowModal}= useContext(ModalContext)
-	return (<>
-		<button
-			className="w-30 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
-			onClick={() => setShowModal(true)} >
-			Set Deadline
-		</button>
-</>
+	const { modalData, setModalData } = useContext(ModalContext)
+	return (
+		<>
+			<button
+				className="w-30 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
+				onClick={() =>
+					setModalData({
+						...modalData,
+						modalShow: true,
+						modalType: 'newDeadline',
+					})
+				}
+			>
+				Set Deadline
+			</button>
+		</>
 	)
 }
 
