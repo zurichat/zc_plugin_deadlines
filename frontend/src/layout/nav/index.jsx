50095e7ec307e1f8848b8f3165458c879f51ddeb
AndrewGlago <andrewglago1@gmail.com>
2021-09-05 10:57:38 +0000

    fix display on md+

--- frontend/src/layout/nav/index.jsx
+++ frontend/src/layout/nav/index.jsx
@@ -5,14 +5,22 @@ import Button from './button'
 
 const Nav = () => {
 	return (
-		<nav className="px-5 space-y-1">
+		<nav className="px-5 space-y-1 md:space-y-0">
 			<div className="flex justify-between py-5 md:w-4/6 items-center">
-				<p className="font-bold text-xl">Deadlines</p>
+				<p className="font-bold text-2xl">Deadlines</p>
+				<div className="hidden md:flex flex-col space-y-2 sm:space-y-0 sm:flex-row">
+					<div className="sm:mx-1 lg:block">
+						<Searchbar />
+					</div>
+					<div className="md:mx-5 md:flex-grow-0">
+						<Sortbar />
+					</div>
+				</div>
 				<div className="flex justify-end md:absolute md:right-6 md:top-5 xl:right-5">
 					<Button />
 				</div>
 			</div>
-			<div className="flex flex-col space-y-2 sm:space-y-0 sm:flex-row">
+			<div className="md:hidden flex flex-col space-y-2 sm:space-y-0 sm:flex-row">
 				<div className="sm:mx-1 lg:block">
 					<Searchbar />
 				</div>
