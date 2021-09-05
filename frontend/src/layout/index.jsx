ba598fa63dfc26416dcbcc3747376d72016a4251
AndrewGlago <andrewglago1@gmail.com>
2021-09-05 21:24:46 +0000

    overflow-y on deadline status

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -25,8 +25,8 @@ const Layout = () => {
 				<div className="md:col-span-2 h-screen/1.5 md:h-screen overflow-y-scroll border-r-2 border-opacity-40 p-6">
 					<DeadlineList reminderArray={reminders ? reminders : null} />
 				</div>
-				<div className="md:col-span-1 p-6 md:h-screen m-4">
-					<DeadlineStatus />
+				<div className="md:col-span-1 p-6 md:h-screen m-4 overflow-y-scroll">
+					<DeadlineStatus reminderArray={reminders ? reminders : null} />
 				</div>
 			</div>
 		</div>
