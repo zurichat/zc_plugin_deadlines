abb5e9eff5b2a8885ecd13156abcfd35f79ebdde
AndrewGlago <andrewglago1@gmail.com>
2021-09-05 22:31:21 +0000

    activity indicator

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 
 import './scroll.css'
-import 'react-activity/dist/Dots.css'
+import 'react-activity/dist/Spinner.css'
 
 import Layout from './layout'
 import { ModalContext } from './context/ModalContext.jsx'
--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -3,7 +3,7 @@ import Nav from './nav'
 
 import DeadlineList from '../deadlineList'
 import DeadlineStatus from '../deadlineStatus'
-import Spinner from 'react-activity'
+import { Spinner } from 'react-activity'
 
 import { useAllReminders } from '@api/reminders'
 
@@ -25,29 +25,23 @@ const Layout = () => {
 	return (
 		<div
 			id="layout-root"
-			className="bg-white w-full h-screen gap-4 p-1 overflow-y-scroll"
+			className="bg-white w-full h-screen gap-4 p-1 overflow-y-scroll flex flex-col px-5"
 		>
 			<Nav />
-			<div className="flex flex-col md:grid md:grid-cols-3 ">
-				<div className="md:col-span-2 h-screen/1.5 md:h-screen overflow-y-scroll border-r-2 border-opacity-40 p-6">
-					{isLoading ? (
-						<div className="w-full h-full flex justify-center align-center">
-							<Spinner color="#00B87C" size={32} speed={1} animating={true} />
-						</div>
-					) : (
+			{isLoading ? (
+				<div className="flex flex-grow justify-center items-center">
+					<Spinner color="#00B87C" size={32} speed={1} animating={true} />
+				</div>
+			) : (
+				<div className="flex flex-col md:grid md:grid-cols-3 ">
+					<div className="md:col-span-2 h-screen/1.5 md:h-screen overflow-y-scroll border-r-2 border-opacity-40 py-6">
 						<DeadlineList reminderArray={reminders ? reminders : null} />
-					)}
-				</div>
-				<div className="md:col-span-1 p-6 md:h-screen m-4 overflow-y-scroll">
-					{isLoading ? (
-						<div className="w-full h-full flex justify-center align-center">
-							<Spinner color="#00B87C" size={32} speed={1} animating={true} />
-						</div>
-					) : (
+					</div>
+					<div className="md:col-span-1 p-6 md:h-screen m-4 overflow-y-scroll">
 						<DeadlineStatus reminderArray={reminders ? reminders : null} />
-					)}
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	)
 }
