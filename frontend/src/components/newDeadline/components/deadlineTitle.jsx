b9382d198a2801740700e634af7544c3db924011
AndrewGlago <andrewglago1@gmail.com>
2021-09-05 00:45:15 +0000

    props and state update

--- frontend/src/components/newDeadline/components/deadlineTitle.jsx
+++ frontend/src/components/newDeadline/components/deadlineTitle.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react'
 
-const DeadlineTitle = () => {
-	const [title, setTitle] = useState('')
+const DeadlineTitle = ({ title, ...props }) => {
+	const [titleState, setTitleState] = useState('')
 	return (
 		<div className="m-5 my-5 text-lg">
 			<label htmlFor="title" className="text-left font-bold">
-				Title
+				{title}
 			</label>
 			<br />
 			<input
 				className="border-b-2 border-gray-200 w-2/5 focus:outline-none focus:border-brand-primary"
 				type="text"
 				id="title"
-				value={title}
-				onChange={(e) => setTitle(e.target.value)}
+				value={titleState}
+				{...props}
+				onChange={(e) => setTitleState(e.target.value)}
 			/>
 		</div>
 	)
