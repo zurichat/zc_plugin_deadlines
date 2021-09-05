a336c3a3009e6a5abc3bfbd6f89560acd2d75aa3
jutivia <76624942+jutivia@users.noreply.github.com>
2021-09-05 06:10:21 +0100

    Update button.jsx

--- frontend/src/layout/nav/button.jsx
+++ frontend/src/layout/nav/button.jsx
@@ -1,8 +1,8 @@
-import React,{useContext} from 'react'
+import React,{useContext, useState} from 'react'
 import NewDeadline from "../../components/newDeadline/index"
 
 const Button = () => {
-
+const [isDeadline, setIsDeadline]= useState(false)
 	return (
 		<button
 			className="w-30 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
