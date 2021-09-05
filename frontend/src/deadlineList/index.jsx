78b537f5cd23d934bd198e3697b789dda1ec1c93
AndrewGlago <andrewglago1@gmail.com>
2021-09-05 19:59:25 +0000

    list card

--- frontend/src/deadlineList/index.jsx
+++ frontend/src/deadlineList/index.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import ListCard from './listCard'
+import ListCard from './components/listCard'
+
+// Prop drilling here, should be improved.
 
 const DeadlineContainer = ({ reminderArray }) => {
-	//Logic to render card multiple times
 	return (
 		<div>
 			{Array.isArray(reminderArray) &&
 				reminderArray.map((value, index) => {
-					const { title, description, expiryDate, startDate } = value.payload
+					const { title, description, dueDate, startDate } = value.payload
 					return (
 						<div className="w-full border-b-1/2 mt-5" key={index}>
 							<ListCard
 								title={title}
 								description={description}
-								expiryDate={expiryDate}
+								dueDate={dueDate}
 								startDate={startDate}
 							/>
 						</div>
