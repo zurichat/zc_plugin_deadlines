42ac7dc34b72359e1cc7cadaaaab7916a1ff2c03
spykelion <ndilionel6@gmail.com>
2021-09-06 18:39:02 +0100

    [UserAvatar]: Fixes #400 refactor image

--- frontend/src/deadlineList/components/userAvatar.jsx
+++ frontend/src/deadlineList/components/userAvatar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Avatar1 from '../../assets/img/png/avatar1.png'
-import Avatar2 from '../../assets/img/png/avatar1.png'
+import Avatar2 from '../../assets/img/png/avatar2.png'
 // import Avatar3 from '@assets/img/png/avatar3.png'
 
 const UserAvatar = ({ ...props }) => {
@@ -10,26 +10,34 @@ const UserAvatar = ({ ...props }) => {
 				className="flex flex-row -space-x-20 justify-center overflow-hidden"
 				{...props}
 			>
-				<img
-					src={Avatar2}
-					className="bg-white overflow-hidden z-10 h-48 w-48 inline-block rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
-					alt="Avatar 1"
-				/>
-				<img
-					src={Avatar1}
-					className="bg-white z-20 h-48 w-48 inline-block rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
-					alt="Avatar 2"
-				/>
-				<img
-					src={Avatar2}
-					className="bg-white z-30 h-48 w-48 inline-block rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
-					alt="Avatar 3"
-				/>
-				<img
-					src={Avatar1}
-					className="bg-white z-40 h-48 w-48 inline-block rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
-					alt="Avatar 4"
-				/>
+				<div className="h-full m-1 mr-2 -ml-3 rounded-full inline-block p-0">
+					<img
+						src={Avatar2}
+						className="bg-white overflow-hidden z-10 rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
+						alt="Avatar 1"
+					/>
+				</div>
+				<div className="h-full  m-1 mr-2 -ml-3 rounded-full inline-block">
+					<img
+						src={Avatar1}
+						className="bg-white overflow-hidden z-10 rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
+						alt="Avatar 1"
+					/>
+				</div>
+				<div className="h-full  m-1 mr-2 -ml-3 rounded-full inline-block">
+					<img
+						src={Avatar2}
+						className="bg-white overflow-hidden z-10 rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
+						alt="Avatar 1"
+					/>
+				</div>
+				<div className="h-full  m-1 mr-2 -ml-3 rounded-full inline-block">
+					<img
+						src={Avatar1}
+						className="bg-white overflow-hidden z-10 rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
+						alt="Avatar 1"
+					/>
+				</div>
 			</div>
 		</>
 	)
