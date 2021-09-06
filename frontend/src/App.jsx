ba58ef0d368b06943d0f0a883bad67a7fcd2f41f
spykelion <ndilionel6@gmail.com>
2021-09-06 13:22:05 +0100

    [userAvatar]: Fixes #400 user avatar stack-border

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,8 @@ import React, { useContext } from 'react'
 import './scroll.css'
 import 'react-activity/dist/Spinner.css'
 
-// import Layout from './layout'
+import Layout from './layout'
 import { ModalContext } from './context/ModalContext.jsx'
-import UserAvatar from './deadlineList/components/userAvatar'
 
 import ViewDeadline from './components/viewDeadline'
 import NewDeadline from './components/newDeadline'
@@ -21,8 +20,7 @@ const App = () => {
 			{modalData.modalShow && modalData.modalType === 'newDeadline' ? (
 				<NewDeadline modalData={modalData} />
 			) : null}
-			{/* <Layout /> */}
-			<UserAvatar />
+			<Layout />
 		</>
 	)
 }
