46351435ccc3c673ecb2d3a73880c9ef7b90cec3
spykelion <ndilionel6@gmail.com>
2021-09-06 19:02:42 +0100

    [UserAvatar]: Fixes #400 refactor image

--- frontend/src/deadlineList/components/userAvatar.jsx
+++ frontend/src/deadlineList/components/userAvatar.jsx
@@ -1,26 +1,46 @@
 import React from 'react'
-import Avatar1 from '@assets/img/png/avatar1.png'
-import Avatar2 from '@assets/img/png/avatar2.png'
-import Avatar3 from '@assets/img/png/avatar3.png'
+import Avatar1 from '../../assets/img/png/avatar1.png'
+import Avatar2 from '../../assets/img/png/avatar2.png'
+// import Avatar3 from '@assets/img/png/avatar3.png'
 
 const UserAvatar = ({ ...props }) => {
 	return (
 		<>
-			<div className="flex flex-row -space-x-10" {...props}>
-				<div className="h-full  m-1 mr-2 -ml-3 rounded-full ">
-					<img src={Avatar1} className="rounded-full" alt="Avatar 1" />
+			<div
+				className="flex flex-row -space-x-20 justify-center overflow-hidden"
+				{...props}
+			>
+				<div className="h-full m-1 mr-2 -ml-3 rounded-full inline-block p-0">
+					<img
+						src={Avatar2}
+						className="bg-white overflow-hidden z-10 rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
+						alt="Avatar 1"
+					/>
 				</div>
-				<div className="bg-brand-avatar-yellow h-full  m-1 mr-2 -ml-3 rounded-full border border-brand-avatar-green">
-					<img src={Avatar2} className="rounded-full" alt="Avatar 2" />
+				<div className="h-full  m-1 mr-2 -ml-3 rounded-full inline-block">
+					<img
+						src={Avatar1}
+						className="bg-white overflow-hidden z-10 rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
+						alt="Avatar 1"
+					/>
 				</div>
-				<div className="bg-brand-avatar-pink h-full  m-1 mr-2 -ml-3 rounded-full border border-brand-avatar-green">
-					<img src={Avatar3} className="rounded-full" alt="Avatar 3" />
+				<div className="h-full  m-1 mr-2 -ml-3 rounded-full inline-block">
+					<img
+						src={Avatar2}
+						className="bg-white overflow-hidden z-10 rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
+						alt="Avatar 1"
+					/>
 				</div>
-				<div className="bg-brand-avatar-yellow h-full  m-1 mr-2 -ml-3 rounded-full border border-brand-avatar-green">
-					<img src={Avatar1} className="rounded-full" alt="Avatar 4" />
+				<div className="h-full  m-1 mr-2 -ml-3 rounded-full inline-block">
+					<img
+						src={Avatar1}
+						className="bg-white overflow-hidden z-10 rounded-full border-8 border-green-500 p-5 pb-0 ring-1 ring-green-600"
+						alt="Avatar 1"
+					/>
 				</div>
 			</div>
 		</>
 	)
 }
+
 export default UserAvatar
