02cc954bc99461646ffa88edb09c86059ad94082
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-09-07 17:50:34 +0100

    updated the code and changed it to a toggle using headlesUi.

--- frontend/src/deadlineList/components/CompletedButton.jsx
+++ frontend/src/deadlineList/components/CompletedButton.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
 
-const CompletedButton = () => {
-	return (
-		<>
-			<button className="border-brand-primary border-1/2 text-brand-text-leftNav px-1 rounded-sm">
-				Ongoing...
-			</button>
-		</>
-	)
+export default function DeadlineCompleteToggle() {
+  const [enabled, setEnabled] = useState(false)
+
+  return (
+    <Switch.Group>
+      <div className="flex items-center">
+        <Switch.Label className="mr-4 text-brand-text-body" passive>Completed</Switch.Label>
+        <Switch
+          checked={enabled}
+          onChange={setEnabled}
+          className={`${
+            enabled ? 'bg-brand-primary' : 'bg-brand-bg-lightGrey'
+          } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+        >
+          <span
+            className={`${
+              enabled ? 'translate-x-6' : 'translate-x-1'
+            } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`}
+          />
+        </Switch>
+      </div>
+    </Switch.Group>
+  )
 }
-
-export default CompletedButton
