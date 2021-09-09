7d53f8b1557cf97835d58396cc8007e0717ee545
King <iyosa14@gmail.com>
2021-09-09 22:23:43 +0100

    api WIP

--- src/config/environment/index.js
+++ src/config/environment/index.js
@@ -9,16 +9,13 @@ dotenv.config()
 
 const env = {
 	PORT: process.env.PORT,
-	BASE_URL: process.env.BASE_URL,
-	API_URL: process.env.API_URL,
+	BASE_URL: '',
+	API_URL: '',
 	ENVIRONMENT: {
 		development: process.env.NODE_ENV === 'development',
 		test: process.env.NODE_ENV === 'test',
 		staging: process.env.NODE_ENV === 'staging',
 		production: process.env.NODE_ENV === 'production',
-	},
-	AUTH: {
-		JWT_SECRET: process.env.JWT_SECRET,
 	},
 	MONGODB: {
 		url: process.env.MONGO_DB_URL,
@@ -26,18 +23,30 @@ const env = {
 	MAIL: {
 		email: process.env.SSN_FOR_ORGS_MAIL,
 	},
-	PLUGIN_ID: 'zc_reminder',
-	ORG_ID: 'darwin_organization',
+	PLUGIN_ID: '',
+	ORG_ID: '',
 	getDevBaseUrl() {
 		const self = env
-		self.BASE_URL = 'https://mock-dbapi.herokuapp.com/api'
 		if (
 			process.env.NODE_ENV === 'development' ||
 			process.env.NODE_ENV === 'test'
 		) {
 			self.API_URL = `http://localhost:${self.PORT}/api/v1`
+			self.BASE_URL = 'https://mock-dbapi.herokuapp.com/api'
+			self.PLUGIN_ID = 'zc_reminder'
+			self.ORG_ID = 'darwin_organization'
+		} else {
+			self.API_URL = `http://localhost:${self.PORT}/api/v1`
+			self.BASE_URL = 'https://api.zuri.chat'
+			self.PLUGIN_ID = '61392d9999bd9e223a37d92a' // DON'T CHANGE
+			self.ORG_ID = '6139b25859842c7444fb01f4' // DON'T CHANGE
 		}
-		return { apiUrl: self.API_URL, mockApiUrl: self.BASE_URL }
+		return {
+			apiUrl: self.API_URL,
+			baseUrl: self.BASE_URL,
+			pluginId: self.PLUGIN_ID,
+			orgId: self.ORG_ID,
+		}
 	},
 }
 
