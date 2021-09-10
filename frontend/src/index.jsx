2d5eabbe2686437fc81b85cf760a078dae6248db
josephden16 <tegajoseph2002@gmail.com>
2021-09-11 00:00:59 +0100

    [Reminders-Context] Closes #462 create reminders context provider

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,6 +5,7 @@ import './index.css'
 
 import App from './App.jsx'
 import { ModalContextProvider } from './context/ModalContext'
+import { RemindersContextProvider } from './context/RemindersContext'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
@@ -12,9 +13,11 @@ const queryClient = new QueryClient()
 ReactDOM.render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<ModalContextProvider>
-				<App />
-			</ModalContextProvider>
+			<RemindersContextProvider>
+				<ModalContextProvider>
+					<App />
+				</ModalContextProvider>
+			</RemindersContextProvider>
 		</QueryClientProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
