a2510fd73145eec0b6c02d90b9936fc7905c07dc
kenechukwu offokansi <koffokansi@gmail.com>
2021-09-12 23:02:44 +0100

    moved components to the modals folder

--- frontend/src/components/modals/component/columnTitleDes.jsx
+++ frontend/src/components/modals/component/columnTitleDes.jsx
@@ -4,9 +4,9 @@ const ColTitleDes = ({ title, writeUp }) => {
 	return (
 		<div className="flex flex-col gap-2 items-start">
 			<div>
-				<h3 className="font-semibold text-base leading-none">{title}</h3>
+				<h3 className="font-semibold text-sm leading-none">{title}</h3>
 			</div>
-			<div className="text-sm leading-none">{writeUp}</div>
+			<div className="text-xs leading-none">{writeUp}</div>
 		</div>
 	)
 }
