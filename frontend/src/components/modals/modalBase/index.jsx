d59d6bbe08d6b1fdd74480f372e07c5034022b8f
AndrewGlago <andrewglago1@gmail.com>
2021-09-14 08:57:00 +0000

    dir update

--- frontend/src/components/modals/modalBase/index.jsx
+++ frontend/src/components/modals/modalBase/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const ModalBase = () => {
+const ModalBase = ({ children, title }) => {
+	console.log('Mounted')
 	return (
-		<div>
-			<div>Reusable Modal</div>
+		<div
+			id="modal-container"
+			className="absolute w-full h-screen flex backdrop-filter backdrop-blur backdrop-brightness-50"
+		>
+			<div className="bg-white w-6/7 md:w-3/5 m-auto rounded-4xl ">
+				<div className="font-medium leading-6 bg-brand-primary p-5 flex items-center justify-between text-white rounded-t-4xl">
+					<h3>{title}</h3>
+				</div>
+				<div id="modal-child" className="">
+					{children}
+				</div>
+			</div>
 		</div>
 	)
 }
