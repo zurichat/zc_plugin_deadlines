846ef6e5fd50f99c79055287860e30d366c89b0e
AndrewGlago <andrewglago1@gmail.com>
2021-09-14 09:17:58 +0000

    close on outside click

--- frontend/src/components/modals/modalBase/index.jsx
+++ frontend/src/components/modals/modalBase/index.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useContext, cloneElement } from 'react'
+import { ModalContext } from '../../../context/ModalContext'
 
 const ModalBase = ({ children, title }) => {
-	console.log('Mounted')
+	const { modalData, setModalData } = useContext(ModalContext)
+	const closeModal = () => setModalData({ ...modalData, modalShow: false })
+
 	return (
 		<div
 			id="modal-container"
 			className="absolute w-full h-screen flex backdrop-filter backdrop-blur backdrop-brightness-50"
 		>
-			<div className="bg-white w-6/7 md:w-3/5 m-auto rounded-4xl ">
-				<div className="font-medium leading-6 bg-brand-primary p-5 flex items-center justify-between text-white rounded-t-4xl">
+			<div
+				id="overlay"
+				onClick={closeModal}
+				className="absolute h-screen w-full -z-10"
+			/>
+			<div className="bg-white w-6/7 md:w-3/5 m-auto rounded-4xl">
+				<div className="font-medium leading-6 bg-brand-primary py-6 px-7  flex items-center justify-between text-white rounded-t-4xl">
 					<h3>{title}</h3>
 				</div>
-				<div id="modal-child" className="">
-					{children}
+				<div id="modal-child" className="px-7 py-8">
+					{/* Pass close function down */}
+					{cloneElement(children, { closeModal })}
 				</div>
 			</div>
 		</div>
