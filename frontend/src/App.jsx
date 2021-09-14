ba0efed4340a5712d3b6fa7cda749e65b14f5acd
Precious Abubakar <misspee007@gmail.com>
2021-09-14 23:43:34 +0100

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Nav from './components/nav'
 import CompletedPage from './pages/Completed/index'
-import OverduePage from './pages/Overdue'
 import UpcomingPage from './pages/Upcoming'
 import { ModalContext } from './context/ModalContext'
 import NewDeadline from './components/__compat__/newDeadline'
@@ -26,9 +25,6 @@ const App = () => {
 					</Route>
 					<Route path="/completed">
 						<CompletedPage />
-					</Route>
-					<Route path="/overdue">
-						<OverduePage />
 					</Route>
 				</Switch>
 			</main>
--- frontend/src/components/nav/__compat__/nav_index.jsx
+++ frontend/src/components/nav/__compat__/nav_index.jsx
@@ -29,35 +29,34 @@ const Navbar = () => {
 				<p className="font-bold text-2xl">Company Deadlines</p>
 			</div>
 
-			<div className="relative flex justify-between">
+			<div className="relative flex justify-between items-center">
 				{/* the onclick events with blue line */}
 				<div className="md:flex md:justify-between">
 					<Link to="/">
 						<NavItem navText="Upcoming" />
 					</Link>
-					<Link to="/overdue" className="hidden text-blue-500 mx-5">
-						<NavItem navText="Overdue" />
-					</Link>
-					<Link to="/completed" className="hidden text-blue-500 mx-5">
+					<Link to="/completed" className="hidden lg:block text-blue-500 mx-5">
 						<NavItem navText="Completed" />
 					</Link>
 				</div>
 
-				<div className="absolute -bottom-2">
+				<div className="absolute -bottom-10 lg:static ">
 					<SetDeadlineButton />
 				</div>
 
-				<div className=" flex ">
-					<SearchIcon className="md:hidden text-gray-400 w-3.5 mr-3" />
+				<div className="flex justify-between ">
+					<div className=" flex ">
+						<SearchIcon className="lg:hidden text-gray-400 w-3.5 mr-3" />
 
-					<FilterIcon className=" md:hidden text-gray-400 w-3.5" />
+						<FilterIcon className=" lg:hidden text-gray-400 w-3.5" />
 
-					<div className="hidden lg:block">
-						<Searchbar />
-					</div>
+						<div className="hidden lg:block px-4">
+							<Searchbar />
+						</div>
 
-					<div className="hidden md:flex-grow-0">
-						<Sortbar />
+						<div className="hidden lg:block ">
+							<Sortbar />
+						</div>
 					</div>
 				</div>
 			</div>
