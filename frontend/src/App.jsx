12e4f03d50367e45664d24bb6733409bf11f9d26
AndrewGlago <andrewglago1@gmail.com>
2021-09-14 08:56:20 +0000

    modal show

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Nav from './components/nav'
 
@@ -6,10 +6,17 @@ import CompletedPage from './pages/Completed/index'
 import OverduePage from './pages/Overdue'
 import UpcomingPage from './pages/Upcoming'
 import Tabs from './components/nav/__compat__/nav_index.jsx'
+import { ModalContext } from './context/ModalContext'
+import NewDeadline from './components/__compat__/newDeadline'
 
 const App = () => {
+	const { modalData } = useContext(ModalContext)
+
 	return (
 		<BrowserRouter>
+			{modalData.modalShow && modalData.modalType === 'adminCreate' && (
+				<NewDeadline />
+			)}
 			<Nav />
 			<Switch>
 				<Route path="/" exact>
