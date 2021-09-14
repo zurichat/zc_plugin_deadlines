c59adef30e27c50aec4b6053def0f9349267bfd9
AndrewGlago <andrewglago1@gmail.com>
2021-09-14 15:15:41 +0000

    date formatting

--- frontend/src/components/deadlineCard/index.jsx
+++ frontend/src/components/deadlineCard/index.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { DateTime } from 'luxon'
+
+import { BsThreeDots } from 'react-icons/bs'
+import { ReactComponent as Date } from '../../assets/svg/date.svg'
+import { ReactComponent as Clock } from '../../assets/svg/clock-group.svg'
 
 const DeadlineCard = ({
 	title,
@@ -18,37 +23,86 @@ const DeadlineCard = ({
 			? 'bg-brand-priority-high'
 			: 'bg-gray'
 
+	const startDateStr = DateTime.fromSeconds(startDate, {
+		zone: 'UTC',
+	}).toLocaleString(DateTime.DATE_MED)
+
+	const dueDateStr = DateTime.fromSeconds(dueDate, {
+		zone: 'UTC',
+	}).toLocaleString(DateTime.DATE_MED)
+
+	const remaining = DateTime.fromSeconds(1632745940)
+		.diffNow(['days', 'hours', 'minutes', 'seconds'])
+		.toObject()
+
+	const remainingStr =
+		remaining['days'] > 0
+			? `${remaining['days']} ${remaining['days'] === 1 ? 'day' : 'days'}`
+			: remaining['hours'] > 0
+			? `${remaining['hours']} ${remaining['hours'] === 1 ? 'hour' : 'hours'}`
+			: remaining['minutes'] > 0
+			? `${remaining['minutes']} ${
+					remaining['minutes'] === 1 ? 'minute' : 'minutes'
+			  }`
+			: remaining['seconds'] > 0
+			? `${remaining['seconds']} ${
+					remaining['seconds'] === 1 ? 'second' : 'seconds'
+			  }`
+			: `Expired`
+
 	return (
 		<div className="ring-1 ring-brand-border ring-opacity-50 flex flex-col p-4 rounded-xl">
 			<div id="header-text" className="mb-4">
-				<div className="flex">
-					<p className="font-semibold text-lg">{title}</p>
-					<div
-						id="priority"
-						className={`w-2 h-2 rounded-full self-center ml-2 ${priority_bg}`}
-					/>
+				<div className="flex justify-between">
+					<div className="flex">
+						<p className="font-semibold text-lg text-brand-text-header">
+							{title}
+						</p>
+						<div
+							id="priority"
+							className={`w-2 h-2 rounded-full self-center ml-2 ${priority_bg}`}
+						/>
+					</div>
+					<BsThreeDots className="self-center place-self-end cursor-pointer text-black text-opacity-50" />
 				</div>
 				<p className="text-brand-text-body text-opacity-50">{`Assigned by ${assigner} to #${assignees}`}</p>
 			</div>
-			<p className="">{description}</p>
+			<p className="text-brand-text-body">{description}</p>
 			<div id="y-divider" className="border-1/2 border-opacity-50 mt-3 mb-4" />
 			<div className="flex">
 				<div id="startDate" className="flex">
+					<div className="mr-2 pt-1">
+						<div className="bg-brand-svg-blue rounded-full p-1">
+							<Date />
+						</div>
+					</div>
 					<div>
-						<p>Start Date:</p>
-						<p>{startDate}</p>
+						<p className="text-brand-text-body text-opacity-60 ">Start Date:</p>
+						<p className="text-brand-text-body">{startDateStr}</p>
 					</div>
 				</div>
+				<div id="x-divider" className="border-1/2 border-opacity-50 mx-3" />
 				<div id="dueDate" className="flex">
+					<div className="mr-2 pt-1">
+						<div className="bg-brand-svg-blue rounded-full p-1">
+							<Date />
+						</div>
+					</div>
 					<div>
-						<p>Due Date:</p>
-						<p>{dueDate}</p>
+						<p className="text-brand-text-body text-opacity-60">Due Date:</p>
+						<p className="text-brand-text-body">{dueDateStr}</p>
 					</div>
 				</div>
+				<div id="x-divider" className="border-1/2 border-opacity-50 mx-3" />
 				<div id="dueIn" className="flex">
+					<div className="mr-2 pt-1">
+						<div className="bg-brand-svg-green rounded-full p-1">
+							<Clock />
+						</div>
+					</div>
 					<div>
-						<p>Due In:</p>
-						<p>{dueDate}</p>
+						<p className="text-brand-text-body text-opacity-60">Due In:</p>
+						<p className="text-brand-text-body">{remainingStr}</p>
 					</div>
 				</div>
 			</div>
