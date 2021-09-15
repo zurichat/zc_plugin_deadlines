cc679c4979963b9222f6c2186706bd5bbaf30e39
Ndi Lionel <59184392+spykelion@users.noreply.github.com>
2021-09-15 10:27:10 +0100

    [adminViewOther]: modified props

--- frontend/src/components/modals/modalBase/adminViewOther.jsx
+++ frontend/src/components/modals/modalBase/adminViewOther.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import ModalBase from './index'
-// import AvatarIcon from '../../../assets/svg/Assigner.svg'
+import AvatarIcon from '../../../assets/svg/Assigner.svg'
 
-// const deadline = {
-// 	title: 'Design Reminder Modals',
-// 	priorityLevel: 'Low',
-// 	description:
-// 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
-// 	startDate: '31st August, 2021',
-// 	dueDate: '1st Sept, 2021',
-// 	dueIn: '24 hours',
-// 	assignedTo: '#channelname',
-// 	assignedBy: 'John Doe',
-// 	assignerAvatar: AvatarIcon,
-// }
 const AdminViewOther = ({
 	title,
 	priorityLevel,
@@ -51,7 +39,7 @@ const AdminViewOther = ({
 				<div className="start mt-6">
 					<h3 className="font-bold">Asssigned By</h3>
 					<div className="flex mt-2">
-						<img src={assignerAvatar} alt="assigner-avatar" />
+						<img src={AvatarIcon} alt="assigner-avatar" />
 						<p className="text-gray-600 pl-5">{assignedBy}</p>
 					</div>
 				</div>
