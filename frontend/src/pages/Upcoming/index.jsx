0c827d9943d0b6a27bdde4d4ec7c6ed232a129e2
josephden16 <tegajoseph2002@gmail.com>
2021-09-15 18:42:03 +0100

    [Bug] Closes #490 fix bug in upcoming deadlines listing component

--- frontend/src/pages/Upcoming/index.jsx
+++ frontend/src/pages/Upcoming/index.jsx
@@ -4,27 +4,36 @@ import DeadlineCard from '../../components/deadlineCard'
 import Skeleton from '../../components/skeleton/skeleton'
 
 const UpcomingPage = () => {
-	const { fetchedData, isPlaceholderData, isLoading } = useAllReminders()
+	const { fetchedData, isPlaceholderData, isLoading, isError } =
+		useAllReminders()
 
 	return !isPlaceholderData && !isLoading ? (
-		<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-			{fetchedData.map((val) => (
-				<div key={val.object_id}>
-					<DeadlineCard
-						title={val.title}
-						description={val.description}
-						assigner={val.creator.userName}
-						assignees={val.assignee.channelName}
-						startDate={val.startDate}
-						dueDate={val.dueDate}
-						priority={val.priority}
-					/>
-				</div>
-			))}
-		</div>
+		isError ? (
+			<div className="text-center font-semibold">
+				An unexpected error occured &#128542;
+			</div>
+		) : (
+			<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+				{Array.isArray(fetchedData) &&
+					fetchedData.length > 0 &&
+					fetchedData.map((val) => (
+						<div key={val.object_id}>
+							<DeadlineCard
+								title={val.title}
+								description={val.description}
+								assigner={val.creator.userName}
+								assignees={val.assignee.channelName}
+								startDate={val.startDate}
+								dueDate={val.dueDate}
+								priority={val.priority}
+							/>
+						</div>
+					))}
+			</div>
+		)
 	) : (
 		<div className="flex h-screen justify-center items-center">
-			<Skeleton counter={3} />
+			<Skeleton counter={9} />
 		</div>
 	)
 }
