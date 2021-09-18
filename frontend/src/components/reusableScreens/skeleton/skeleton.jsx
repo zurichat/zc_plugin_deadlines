12c3c9a779bfbc08af099c8ffab60ed4a1ba1bd3
Adetoba Adedeji <adetoba22@gmail.com>
2021-09-18 15:46:12 +0100

    fixed the key error in the Array and re-created the skeleton to fit the new design

--- frontend/src/components/reusableScreens/skeleton/skeleton.jsx
+++ frontend/src/components/reusableScreens/skeleton/skeleton.jsx
@@ -12,41 +12,38 @@ const Skeleton = ({ type, counter }) => {
 	)
 
 	const CardSkeleton = () => (
-		<div className="bg-gray-white border-2 p-4 rounded-2xl">
-			<div className="flex h-5 flex-row justify-between mb-2 items-center">
-				<div className="w-1/3 h-full bg-gray-200 animate-pulse rounded"></div>
-				<div className="w-5 h-1/2 bg-gray-200 animate-pulse rounded"></div>
+		<div className="flex flex-row bg-gray-white border-2 p-5 rounded-2xl">
+			<div className="w-2/3 pr-4 border-r-2">
+				<div className="w-2/3 h-5 mb-2 bg-gray-200 animate-pulse rounded"></div>
+				<div className="w-full h-3 bg-gray-200 mb-5 animate-pulse rounded"></div>
+				<div className="w-full h-3 bg-gray-200 mb-1 animate-pulse rounded"></div>
+				<div className="w-full h-3 bg-gray-200 mb-1 animate-pulse rounded"></div>
+				<div className="w-full h-3 bg-gray-200 mb-5 animate-pulse rounded"></div>
+				<div className="w-2/4 h-7 bg-gray-200 mb-1 animate-pulse rounded"></div>
 			</div>
-			<div className="w-2/3 h-3 bg-gray-200 mb-5 animate-pulse rounded"></div>
-			<div className="w-2/3 h-3 bg-gray-200 mb-1 animate-pulse rounded"></div>
-			<div className="w-2/3 h-3 bg-gray-200 mb-3 animate-pulse rounded"></div>
-			<div className="w-full h-1 bg-gray-200 mb-3 animate-pulse rounded"></div>
-			<div className="w-full h-14 space-x-2 flex flex-row rounded">
-				<div className="w-1/3 h-full bg-gray-50 flex flex-row justify-between px-2 py-2 rounded">
+
+			<div className="w-1/3 flex flex-col space-y-2 ">
+				<div className="w-5 h-2 ml-auto bg-gray-200 animate-pulse mb-3 rounded-sm"></div>
+
+				<div className="w-full  flex flex-row justify-between p-2 rounded">
 					<div className="w-1/3 flex flex-col items-center ">
-						<div className="w-5 h-5 bg-gray-300 animate-pulse rounded-full"></div>
+						<div className="w-5 h-5 bg-gray-200 animate-pulse rounded-full"></div>
 					</div>
 					<div className="w-2/3 flex flex-col space-y-2">
-						<div className="w-2/3 h-3 bg-gray-300 animate-pulse rounded"></div>
-						<div className="w-full h-3 bg-gray-300 animate-pulse rounded"></div>
+						<div className="w-2/3 h-3 bg-gray-200 animate-pulse rounded"></div>
+						<div className="w-full h-3 bg-gray-200 animate-pulse rounded"></div>
 					</div>
 				</div>
-				<div className="w-1/3 h-full bg-gray-50 flex flex-row justify-between px-2 py-2 rounded">
+
+				<div className="w-full h-px ml-2 bg-gray-100"></div>
+
+				<div className="w-full  flex flex-row justify-between p-2 rounded">
 					<div className="w-1/3 flex flex-col items-center ">
-						<div className="w-5 h-5 bg-gray-300 animate-pulse rounded-full"></div>
+						<div className="w-5 h-5 bg-gray-200 animate-pulse rounded-full"></div>
 					</div>
 					<div className="w-2/3 flex flex-col space-y-2">
-						<div className="w-2/3 h-3 bg-gray-300 animate-pulse rounded"></div>
-						<div className="w-full h-3 bg-gray-300 animate-pulse rounded"></div>
-					</div>
-				</div>
-				<div className="w-1/3 h-full bg-gray-50 flex flex-row justify-between px-2 py-2 rounded">
-					<div className="w-1/3 flex flex-col items-center ">
-						<div className="w-5 h-5 bg-gray-300 animate-pulse rounded-full"></div>
-					</div>
-					<div className="w-2/3 flex flex-col space-y-2">
-						<div className="w-2/3 h-3 bg-gray-300 animate-pulse rounded"></div>
-						<div className="w-full h-3 bg-gray-300 animate-pulse rounded"></div>
+						<div className="w-2/3 h-3 bg-gray-200 animate-pulse rounded"></div>
+						<div className="w-full h-3 bg-gray-200 animate-pulse rounded"></div>
 					</div>
 				</div>
 			</div>
@@ -57,18 +54,23 @@ const Skeleton = ({ type, counter }) => {
 	if (type === 'card')
 		return (
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-				{Array(COUNTER).fill(<CardSkeleton />)}
+				{[...Array(COUNTER)].map((n, index) => (
+					<CardSkeleton key={index} n={n} />
+				))}
 			</div>
 		)
 
-	return (
-		<div className="w-full h-full">
-			{/* <TopMenuSkeleton /> */}
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-				{Array(COUNTER).fill(<CardSkeleton />)}
+	if (type !== ('top' && 'card'))
+		return (
+			<div className="w-full p-5  min-h-screen">
+				<TopMenuSkeleton />
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+					{[...Array(COUNTER)].map((n, index) => (
+						<CardSkeleton key={index} n={n} />
+					))}
+				</div>
 			</div>
-		</div>
-	)
+		)
 }
 
 export default Skeleton
