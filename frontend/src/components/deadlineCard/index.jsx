1ecc71e6ad35aba2979241562780bcf59da1ca13
AndrewGlago <andrewglago1@gmail.com>
2021-09-19 01:49:30 +0000

    card update

--- frontend/src/components/deadlineCard/index.jsx
+++ frontend/src/components/deadlineCard/index.jsx
@@ -80,68 +80,83 @@ const DeadlineCard = ({
 	}
 	return (
 		<div
-			className="ring-1 ring-brand-border ring-opacity-50 flex flex-col p-4 rounded-xl"
+			className="ring-1 ring-brand-border ring-opacity-50 flex p-4 rounded-xl min-h-full justify-evenly"
 			onClick={UserViewDeadline}
 		>
-			<div id="header-text" className="mb-4">
-				<div className="flex justify-between">
-					<div className="flex">
-						<p className="font-semibold text-lg text-brand-text-header">
-							{title}
-						</p>
-						<div
-							id="priority"
-							className={`w-2 h-2 rounded-full self-center ml-2 ${priority_bg}`}
-						/>
-					</div>
-					<BsThreeDots className="self-center place-self-end cursor-pointer text-black text-opacity-50" />
-				</div>
-				<p className="text-brand-text-body text-opacity-50">{`Assigned by ${assigner} to #${assignees}`}</p>
-			</div>
-			<p className="text-brand-text-body">{description}</p>
-			<div id="y-divider" className="border-1/2 border-opacity-50 mt-3 mb-4" />
-			<div className="flex">
-				<div id="startDate" className="flex">
-					<div className="mr-2 pt-1">
-						<div className="bg-brand-svg-blue rounded-full p-1">
-							<Date />
+			<div className="w-card-w">
+				<div id="header-text" className="">
+					<div className="flex justify-between">
+						<div className="flex">
+							<p className="font-semibold md:text-lg text-brand-text-header">
+								{title}
+							</p>
+							<div
+								id="priority"
+								className={`min-w-priority h-2 rounded-full self-center ml-1 md:ml-2 ${priority_bg}`}
+							/>
 						</div>
 					</div>
-					<div>
-						<p className="text-brand-text-body text-opacity-60 ">Start Date:</p>
-						<p className="text-brand-text-body">{startDateStr}</p>
-					</div>
+					<p className="text-brand-text-body text-opacity-50 text-sm md:text-base">{`Assigned by ${assigner} to #${assignees}`}</p>
 				</div>
-				<div id="x-divider" className="border-1/2 border-opacity-50 mx-3" />
-				<div id="dueDate" className="flex">
-					<div className="mr-2 pt-1">
-						<div className="bg-brand-svg-blue rounded-full p-1">
-							<Date />
-						</div>
-					</div>
-					<div>
-						<p className="text-brand-text-body text-opacity-60">Due Date:</p>
-						<p className="text-brand-text-body">{dueDateStr}</p>
-					</div>
-				</div>
-				<div id="x-divider" className="border-1/2 border-opacity-50 mx-3" />
-				<div id="dueIn" className="flex">
-					<div className="mr-2 pt-1">
-						<div className="bg-brand-svg-green rounded-full p-1">
+				<p className="text-brand-text-body mt-4 mb-6 text-sm md:text-base line-clamp-2">
+					{description}
+				</p>
+				<div id="dueIn" className="flex items-center">
+					<div className="mr-2">
+						<div className="bg-brand-svg-green rounded-full p-1 w-5 h-5 flex items-center justify-center">
 							<Clock />
 						</div>
 					</div>
+					<b className="text-brand-text-body text-xs xs:text-sm md:text-base">
+						Due In:
+					</b>
+					&nbsp;
+					<span
+						className={
+							DateTime.fromISO(dueDate).diffNow().as('hours') < 3
+								? 'text-brand-text-overdue text-xs xs:text-sm md:text-base'
+								: 'text-brand-text-body text-xs xs:text-sm md:text-base'
+						}
+					>
+						{remainingStr}
+					</span>
+				</div>
+			</div>
+			<div id="y-divider" className="border-1/2 border-opacity-50 mx-4" />
+			<div className="flex flex-col justify-between">
+				<BsThreeDots className="self-end mb-2 cursor-pointer text-black text-opacity-50" />
+				<div id="startDate" className="flex">
+					<div className="mr-2 pt-1">
+						<div className="bg-brand-svg-blue rounded-full p-1 w-5 h-5 flex items-center justify-center">
+							<Date />
+						</div>
+					</div>
 					<div>
-						<p className="text-brand-text-body text-opacity-60">Due In:</p>
-						<p
-							className={
-								DateTime.fromISO(dueDate).diffNow().as('hours') < 3
-									? 'text-brand-text-overdue'
-									: 'text-brand-text-body'
-							}
-						>
-							{remainingStr}
+						<p className="text-brand-text-body text-opacity-60 text-xs md:text-sm">
+							Start Date:
 						</p>
+						<b className="text-brand-text-body text-xs md:text-sm">
+							{startDateStr}
+						</b>
+					</div>
+				</div>
+				<div
+					id="y-divider"
+					className="border-1/2 border-opacity-50 w-full mt-2 mb-6"
+				/>
+				<div id="dueDate" className="flex">
+					<div className="mr-2 pt-1">
+						<div className="bg-brand-svg-blue rounded-full p-1 w-5 h-5 flex items-center justify-center">
+							<Date />
+						</div>
+					</div>
+					<div>
+						<p className="text-brand-text-body text-opacity-60 text-xs md:text-sm">
+							Due Date:
+						</p>
+						<b className="text-brand-text-body text-xs md:text-sm">
+							{dueDateStr}
+						</b>
 					</div>
 				</div>
 			</div>
