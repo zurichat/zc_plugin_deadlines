b67edfacd40d2f005c82b348b540b029ddafd242
ekaji <ekajionah@gmail.com>
2021-09-19 17:31:41 +0100

    changes to searchModal

--- frontend/src/components/modals/searchModal/searchModal.jsx
+++ frontend/src/components/modals/searchModal/searchModal.jsx
@@ -4,11 +4,23 @@ import { SearchContext } from '../../../context/searchContext/searchContext'
 
 const SearchModal = () => {
 	const searchResult = useContext(SearchContext)
-	console.log(searchResult[0], 'searchModal')
-	// const response = searchResult[0]
+	console.log(searchResult[1], 'searchModal')
 	return (
 		<>
-			<DeadlineCard searchResult={searchResult[0]} />
+			{searchResult[0].map((result) => (
+				<div key={result.object_id}>
+					<DeadlineCard
+						title={result.title}
+						description={result.description}
+						assigner={result.creator.userName}
+						assignees={result.assignee.channelName}
+						startDate={result.startDate}
+						dueDate={result.dueDate}
+						priority={result.priority}
+						object_id={result.object_id}
+					/>
+				</div>
+			))}
 		</>
 	)
 }
