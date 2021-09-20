03f734a5ea63b9d2c0bdca399ed6e5805ae87524
Bukola <bukolaebikefe@yahoo.com>
2021-09-20 20:22:38 +0100

    [sortbar] worked on functionality

--- frontend/src/components/nav/components/sortbar.jsx
+++ frontend/src/components/nav/components/sortbar.jsx
@@ -1,25 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { useQueryClient } from 'react-query'
 
 import { Menu } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
-// import { RemindersContext } from '../../../context/RemindersContext'
+import { RemindersContext } from '../../../context/RemindersContext'
 
 const Sortbar = () => {
+	const { initialData, fetchedData } = useContext(RemindersContext)
+	console.log(initialData)
+	console.log(fetchedData)
 	const sort_tags = [
-		'Piority',
 		'Newest to Oldest',
 		'Oldest to Newest',
 		'Due Date (Ascending)',
 		'Due Date (Descending)',
 	]
-	// const { dispatch, actionTypes, initialData } = useContext(RemindersContext)
-	const [value] = useState('')
+	const [value, setValue] = useState('')
+	const queryClient = useQueryClient()
 
-	//  const handleSort = (i) => {
-	// 	setValue(i)
-	// 	const getDate = (a) => {
-	// 		return new Date(a).getTime()
-	// 	}
+	const handleSort = (i) => {
+		setValue(i)
+		const getDate = (a) => {
+			const date = new Date(a)
+			return date.getTime(a)
+		}
+
+		const sortData = (value) => {
+			switch (value) {
+				case 'Newest to Oldest':
+					queryClient.setQueryData('allReminders', (array) =>
+						[...array].sort(
+							(a, b) => getDate(a.startDate) - getDate(b.startDate)
+						)
+					)
+				case 'Oldest to Newest':
+					queryClient.setQueryData('allReminders', (array) =>
+						[...array].sort(
+							(a, b) => getDate(b.startDate) - getDate(a.startDate)
+						)
+					)
+				case 'Due Date (Ascending)':
+					queryClient.setQueryData('allReminders', (array) =>
+						[...array].sort((a, b) => getDate(a.dueDate) - getDate(b.dueDate))
+					)
+				case 'Due Date (Descending)':
+					queryClient.setQueryData('allReminders', (array) =>
+						[...array].sort((a, b) => getDate(b.dueDate) - getDate(b.dueDate))
+					)
+			}
+		}
+		sortData(value)
+	}
 
 	// 	const sortData = (array, value) => {
 	// 		switch (value) {
@@ -51,7 +82,7 @@ const Sortbar = () => {
 	return (
 		<Menu as="div" className="text-sm">
 			<div>
-				<Menu.Button className="border rounded py-2.5 px-5 inline-flex items-center">
+				<Menu.Button className="border rounded py-2.5 px-3 inline-flex items-center">
 					<p className="text-gray-400 pr-3">{`Sort by: ${
 						value || 'Newest to Oldest'
 					}`}</p>
@@ -63,7 +94,7 @@ const Sortbar = () => {
 			</div>
 			<Menu.Items className="absolute mt-2 divide-y rounded-lg bg-white ring-1 ring-black ring-opacity-5 inline-flex flex-col shadow-lg">
 				{sort_tags.map((i, index) => (
-					<Menu.Item key={index} onClick={() => null}>
+					<Menu.Item key={index} onClick={() => handleSort(i)}>
 						{({ active }) => (
 							<div
 								className={`${
