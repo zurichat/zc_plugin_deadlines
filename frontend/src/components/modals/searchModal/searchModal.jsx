eeea7266f40bd7d45d8a719b1fdfd797b7f8fab5
ekaji <ekajionah@gmail.com>
2021-09-20 09:46:49 +0100

    search modal and search all setup

--- frontend/src/components/modals/searchModal/searchModal.jsx
+++ frontend/src/components/modals/searchModal/searchModal.jsx
@@ -4,11 +4,15 @@ import { SearchContext } from '../../../context/searchContext/searchContext'
 
 const SearchModal = () => {
 	const searchResult = useContext(SearchContext)
-	console.log(searchResult, 'searchModal')
 	return (
-		<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-			{searchResult[0]
-				? searchResult[0].map((result) => (
+		<div
+			className={`${
+				searchResult[0] ? 'absolute w-full min-h-full bg-white left-0' : ' '
+			}`}
+		>
+			{searchResult[0] ? (
+				<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-10 ml-5">
+					{searchResult[0].map((result) => (
 						<div key={result.object_id}>
 							<DeadlineCard
 								title={result.title}
@@ -21,22 +25,9 @@ const SearchModal = () => {
 								object_id={result.object_id}
 							/>
 						</div>
-				  ))
-				: null}
-			{/* {searchResult.map((result) => (
-				<div key={result.object_id}>
-					<DeadlineCard
-						title={result.title}
-						description={result.description}
-						// assigner={result.creator.userName}
-						// assignees={result.assignee.channelName}
-						startDate={result.startDate}
-						dueDate={result.dueDate}
-						priority={result.priority}
-						object_id={result.object_id}
-					/>
+					))}
 				</div>
-			))} */}
+			) : null}
 		</div>
 	)
 }
--- frontend/src/components/nav/components/searchbar.jsx
+++ frontend/src/components/nav/components/searchbar.jsx
@@ -2,45 +2,17 @@ import { React, useContext } from 'react'
 import { SearchIcon } from '@heroicons/react/solid'
 import { useAllReminders } from '../../../api/reminders'
 import { SearchContext } from '../../../context/searchContext/searchContext'
-
-// export const searchFunction = (searchTerm, fetchedData) => {
-// 	console.log(typeof searchTerm, searchTerm)
-// 	if (searchTerm !== '') {
-// 		const result = fetchedData.filter((data) => {
-// 			return (
-// 				data.title.includes(searchTerm.toString().toLowerCase()) ||
-// 				data.title.includes(searchTerm.toString().toUpperCase()) ||
-// 				data.description.includes(searchTerm.toString().toLowerCase()) ||
-// 				data.description.includes(searchTerm.toString().toUppercase()) ||
-// 				data.assignee.includes(searchTerm.toString().toLowerCase()) ||
-// 				data.assignee.includes(searchTerm.toString().toUpperCase()) ||
-// 				data.title.includes(searchTerm.toString()) ||
-// 				data.description.includes(searchTerm.toString()) ||
-// 				data.assignee.includes(searchTerm.toString())
-// 			)
-// 		})
-
-// 		return result
-// 	}
-// }
-
 export const Searchbar = ({ ...props }) => {
-	const { fetchedData, isPlaceholderData } = useAllReminders()
-	// const [input, setInput] = useState('')
+	const { fetchedData } = useAllReminders()
 	const [searchResult, setSearchResult] = useContext(SearchContext)
 
 	const handleChange = (e) => {
-		// if (!isPlaceholderData && fetchedData && e.target.value !== '') {
 		const filtered = fetchedData.filter((val) => {
 			const regex = new RegExp(`${e.target.value}`, 'ig')
-			console.log(e.target.value)
 			return val.title.match(regex) || val.description.match(regex)
 		})
 		setSearchResult(filtered)
-		console.log(filtered, 'searchbar.jsx')
-		// console.log(searchResult)
-		// }
-		if (e.target.value === '') {
+		if (searchResult && e.target.value === '') {
 			setSearchResult(null)
 		}
 	}
@@ -55,7 +27,6 @@ export const Searchbar = ({ ...props }) => {
 					className="text-gray-400 leading-tight text-sm focus:text-black focus:outline-none mr-5"
 					placeholder="Search"
 					type="text"
-					//value={input}
 					onChange={(e) => handleChange(e)}
 					id="task search"
 				/>
