731e461bb76b81665a8caa98398ad3105726332b
MaryAyokanmiOre <maryoredynamic24@gmail.com>
2021-09-20 23:12:18 +0100

    responsive page update through tweaking of button for different screen sizes with tailwind classes

--- frontend/src/components/nav/components/button.jsx
+++ frontend/src/components/nav/components/button.jsx
@@ -6,7 +6,20 @@ const CreateNewDeadlineButton = (admin) => {
 	return admin ? (
 		<>
 			<button
-				className="w-30 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
+				className="w-30 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded 'sm': '640px',
+                // => @media (min-width: 640px) { ... }
+          
+                'md': '768px',
+                // => @media (min-width: 768px) { ... }
+          
+                'lg': '1024px',
+                // => @media (min-width: 1024px) { ... }
+          
+                'xl': '1280px',
+                // => @media (min-width: 1280px) { ... }
+          
+                '2xl': '1536px',
+                // => @media (min-width: 1536px) { ... }"
 				onClick={() =>
 					setModalData({
 						...modalData,
