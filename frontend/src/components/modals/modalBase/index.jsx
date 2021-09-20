65d6b5949f9e64207f3abe830eaeebfc64afee4b
AndrewGlago <andrewglago1@gmail.com>
2021-09-20 13:55:16 +0000

    modal positioning

--- frontend/src/components/modals/modalBase/index.jsx
+++ frontend/src/components/modals/modalBase/index.jsx
@@ -8,14 +8,14 @@ const ModalBase = ({ children, title }) => {
 	return (
 		<div
 			id="modal-container"
-			className="fixed top-0 w-full h-screen flex backdrop-filter backdrop-blur backdrop-brightness-50 z-50"
+			className="fixed top-0 w-full h-screen flex justify-center items-center backdrop-filter backdrop-blur backdrop-brightness-50 z-50"
 		>
 			<div
 				id="overlay"
 				onClick={closeModal}
 				className="absolute h-screen w-full -z-10"
 			/>
-			<div className="bg-white w-6/7 md:w-3/5 m-auto">
+			<div className="bg-white w-6/7 md:w-3/5">
 				<div className="font-medium leading-6 py-6 px-7 flex items-center justify-between">
 					<h3>{title}</h3>
 				</div>
