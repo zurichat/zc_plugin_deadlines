4ed9b10bb2de93772a3d8ed8abe1a24a50e3ddd9
Haastrup Elijah <haastrupea@gmail.com>
2021-09-20 19:26:12 +0100

    Revert back to andrew cors fix on the server

--- src/shared/http/server.js
+++ src/shared/http/server.js
@@ -17,9 +17,24 @@ const publicPath = path.resolve('frontend', 'public')
 
 const app = express()
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	)
+	next()
+})
+
 app.options('*', cors())
 
-app.use(cors())
+app.use(
+	cors({
+		origin: '*',
+	})
+)
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(build))
