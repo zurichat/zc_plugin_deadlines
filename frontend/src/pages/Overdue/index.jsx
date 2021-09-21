a5a88b7f51febb51646d53c59c7ee40073c23df0
AndrewGlago <andrewglago1@gmail.com>
2021-09-21 08:44:57 +0000

    husky checks

--- frontend/src/pages/Overdue/index.jsx
+++ frontend/src/pages/Overdue/index.jsx
@@ -9,6 +9,7 @@ const OverduePage = () => {
 		useAllReminders()
 	const { sort } = useContext(SortContext)
 
+	const test = 'test'
 	const date = new Date()
 	const dateNow = date.toISOString()
 	const getDate = (a) => {
